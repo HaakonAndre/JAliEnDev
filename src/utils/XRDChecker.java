f62ecdf5d5f77755d95bb7eda6b555de48a4022d
costing <costing@ac58307a-7d81-4c66-b533-6213255c5d0c>
2011-04-15 09:44:33 +0000

    fully check the consistency of all replicas of an lfn

--- src/utils/XRDChecker.java
+++ src/utils/XRDChecker.java
@@ -1,16 +1,20 @@
 package utils;
 
+import java.io.File;
 import java.io.IOException;
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.Map;
 import java.util.Set;
 
 import alien.catalogue.GUID;
 import alien.catalogue.GUIDUtils;
 import alien.catalogue.LFN;
+import alien.catalogue.LFNUtils;
 import alien.catalogue.PFN;
 import alien.catalogue.access.AccessType;
 import alien.catalogue.access.AuthorizationFactory;
+import alien.io.IOUtils;
 import alien.io.protocols.XRDStatus;
 import alien.io.protocols.Xrootd;
 
@@ -78,6 +82,83 @@ public class XRDChecker {
 			return null;
 		
 		return check(guid);
+	}
+	
+	/**
+	 * @param pfn
+	 * @return the check status
+	 */
+	public static final XRDStatus checkByDownloading(final PFN pfn){
+		final Xrootd xrootd = new Xrootd();
+		
+		xrootd.setTimeout(60);
+		
+		File f = null;
+		
+		final GUID guid = pfn.getGuid();
+		
+		try{
+			f = File.createTempFile("xrdstatus", "download");
+		
+			f.delete();
+			
+			System.err.println("Getting this file "+pfn.pfn);
+			
+			xrootd.get(pfn, f);
+			
+			if (f.length() != guid.size){
+				return new XRDStatus(false, "Size is different: catalog="+guid.size+", downloaded size: "+f.length());
+			}
+			
+			final String fileMD5 = IOUtils.getMD5(f);
+			
+			if (!fileMD5.equalsIgnoreCase(guid.md5)){
+				return new XRDStatus(false, "MD5 is different: catalog="+guid.md5+", downloaded file="+fileMD5);
+			}
+		}
+		catch (IOException ioe){
+			return new XRDStatus(false, ioe.getMessage());
+		}
+		finally{
+			if (f!=null)
+				f.delete();
+		}
+		
+		return new XRDStatus(true, null);
+	}
+	
+	/**
+	 * Check all replicas of an LFN, first just remotely querying the status then fully downloading each
+	 * replica and computing the md5sum.
+	 * 
+	 * @param lfn
+	 * @return the status of all replicas
+	 */
+	public static final Map<PFN, XRDStatus> fullCheckLFN(final String lfn){
+		final Map<PFN, XRDStatus> check = XRDChecker.check(LFNUtils.getLFN(lfn));
+
+		if (check==null || check.size()==0)
+			return check;
+		
+		final Iterator<Map.Entry<PFN, XRDStatus>> it = check.entrySet().iterator();
+		
+		while (it.hasNext()){
+			final Map.Entry<PFN, XRDStatus> entry = it.next();
+			
+			final PFN pfn = entry.getKey();
+			
+			final XRDStatus status = entry.getValue();
+			
+			if (status.commandOK){
+				// really ?
+				final XRDStatus downloadStatus = checkByDownloading(pfn);
+				
+				if (!downloadStatus.commandOK)
+					entry.setValue(downloadStatus);
+			}
+		}
+		
+		return check;
 	}
 	
 }
