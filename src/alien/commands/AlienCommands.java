5adbb9cb55005637c770c0aa17a4a64a5a5bff40
alina <alina@ac58307a-7d81-4c66-b533-6213255c5d0c>
2011-05-12 12:16:49 +0000

    name of the implemented commands

--- src/alien/commands/AlienCommands.java
+++ src/alien/commands/AlienCommands.java
@@ -3,7 +3,18 @@ package alien.commands;
 import java.security.Principal;
 import java.util.ArrayList;
 
+/**
+ * @author Alina Grigoras
+ * Alien commands factory
+ */
 public class AlienCommands {
+	/**
+	 * Returns a implementation of the command
+	 * @param Alien principal received from request
+	 * @param array of arguments received from the request
+	 * @return AlienCommand implementation for the requested command
+	 * @throws Exception
+	 */
 	public static AlienCommand getAlienCommand(Principal p, ArrayList<Object> al) throws Exception{
 		if(p == null)
 			throw new SecurityException("No Alien Principal! We hane no credentials");
@@ -17,6 +28,26 @@ public class AlienCommands {
 			if("ls".equals(sLocalCommand))
 				return new AlienCommandls(p, al);
 			else return null;
+		}
+	}
+
+	/**
+	 * @param Alien principal
+	 * @param array of arguments received from the soap request
+	 * @return the name of the requested command
+	 * @throws Exception
+	 */
+	public static String getAlienCommandString(Principal p, ArrayList<Object> al) throws Exception {
+		if(p == null)
+			throw new SecurityException("No Alien Principal! We hane no credentials");
+		
+		if(al.size() < 3){
+			throw new Exception("Alien Command did not receive minimum number of arguments (in this order): username, current directory, command (+ arguments)? ");
+		}
+		else{
+			String sLocalCommand = (String) al.get(2);
+			
+			return sLocalCommand;
 		}
 	}
 }
