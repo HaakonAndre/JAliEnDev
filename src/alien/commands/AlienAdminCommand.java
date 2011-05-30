e40e171137fe450655532ffdc5dd3381f799cd67
steffen <steffen@ac58307a-7d81-4c66-b533-6213255c5d0c>
2011-05-30 20:57:49 +0000

    second constructor

--- src/alien/commands/AlienAdminCommand.java
+++ src/alien/commands/AlienAdminCommand.java
@@ -2,6 +2,8 @@ package alien.commands;
 
 import java.util.List;
 
+import lazyj.Log;
+
 import alien.user.AliEnPrincipal;
 
 public class AlienAdminCommand  extends AlienCommand{
@@ -13,6 +15,14 @@ public class AlienAdminCommand  extends AlienCommand{
 			throw new SecurityException("You need to be admin to execute this command");
 	}
 
+
+	public AlienAdminCommand (final AliEnPrincipal pAlienPrincipal, final String sUsername, final String sCurrentDirectory, final String sCommand, 
+			final int iDebugLevel,final List<?> alArguments) throws Exception{
+            super(pAlienPrincipal,sUsername,sCurrentDirectory,sCommand,iDebugLevel,alArguments);
+        	if(!pAlienUser.canBecome("admin") || super.sUsername!="admin")
+    			throw new SecurityException("You need to be admin to execute this command");
+	}
+	
 	
 	@Override
 	public Object executeCommand() throws Exception {
