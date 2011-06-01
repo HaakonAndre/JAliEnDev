eeae29ab8379a7444106839e4849b935b7147070
costing <costing@ac58307a-7d81-4c66-b533-6213255c5d0c>
2011-06-01 15:32:24 +0000

    initial commit

---
+++ src/alien/shell/SimpleServer.java
@@ -0,0 +1,142 @@
+package alien.shell;
+
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import alien.config.ConfigUtils;
+import alien.ui.Request;
+
+/**
+ * @author costing
+ *
+ */
+public class SimpleServer extends Thread {
+
+	/**
+	 * Logger
+	 */
+	static transient final Logger logger = ConfigUtils.getLogger(SimpleServer.class.getCanonicalName());
+	
+	/**
+	 * The entire connection
+	 */
+	private final Socket connection;
+	
+	/**
+	 * Getting requests by this stream
+	 */
+	private final ObjectInputStream ois;
+	
+	/**
+	 * Writing replies here
+	 */
+	private final ObjectOutputStream oos;
+	
+	private SimpleServer(final Socket connection) throws IOException {
+		this.connection = connection;
+		
+		this.oos = new ObjectOutputStream(connection.getOutputStream());
+		this.oos.flush();
+		
+		this.ois = new ObjectInputStream(connection.getInputStream());
+		
+		setName(connection.getInetAddress().toString());
+		setDaemon(true);
+	}
+	
+	@Override
+	public void run() {
+		long lLasted = 0;
+		
+		long lSerialization = 0;
+		
+		try{
+			while (true){
+				final Object o = ois.readObject();
+				
+				if (o!=null){					
+					if (o instanceof Request){
+						final Request r = (Request) o;
+						
+						//System.err.println("Got request "+o+" from "+r.getRequesterIdentity());
+						
+						long lStart = System.currentTimeMillis();
+						
+						r.run();
+						
+						lLasted += (System.currentTimeMillis() - lStart);
+						
+						long lSer = System.currentTimeMillis();
+						
+						oos.writeObject(r);
+						oos.flush();
+						
+						lSerialization += System.currentTimeMillis() - lSer;
+					}
+					else{
+						logger.log(Level.WARNING, "I don't know what to do with an object of type "+o.getClass().getCanonicalName());
+					}
+				}
+			}
+		}
+		catch (Exception e){
+			System.err.println("Lasted : "+lLasted+", serialization : "+lSerialization);
+			
+			if (ois!=null){
+				try{
+					ois.close();
+				}
+				catch (IOException ioe){
+					// ignore
+				}
+			}
+			
+			if (oos!=null){
+				try{
+					oos.close();
+				}
+				catch (IOException ioe){
+					// ignore
+				}
+			}
+			
+			if (connection!=null){
+				try{
+					connection.close();
+				}
+				catch (IOException ioe){
+					// ignore
+				}
+			}
+		}
+	}
+	
+	/**
+	 * @param args
+	 * @throws IOException
+	 */
+	public static void main(final String[] args) throws IOException{
+		final ServerSocket ss = new ServerSocket(5282);
+		
+		logger.log(Level.INFO, "Server listening on "+ss.getLocalPort());
+		
+		while (true){
+			try{
+				final Socket s = ss.accept();
+			
+				logger.log(Level.INFO, "Got a connection from : "+s.getInetAddress());
+				
+				new SimpleServer(s).start();
+			}
+			catch (IOException ioe){
+				logger.log(Level.WARNING, "Exception treating a client", ioe);
+			}
+		}
+	}
+	
+}
