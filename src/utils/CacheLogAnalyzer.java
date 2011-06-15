b4be9bd729eeee1b84dfb2eb3a256a3c928b6b38
costing <costing@ac58307a-7d81-4c66-b533-6213255c5d0c>
2011-06-15 21:53:30 +0000

    found bug

--- src/utils/CacheLogAnalyzer.java
+++ src/utils/CacheLogAnalyzer.java
@@ -80,7 +80,7 @@ public class CacheLogAnalyzer {
 		String image = key.substring(idx+1);
 		
 		incStats("access", "image_no", image, hit);
-		System.out.println("Incrementing image_no "+image+" with "+hit);
+		//System.out.println("Incrementing image_no "+image+" with "+hit);
 		
 		int idx2 = key.lastIndexOf('_', idx-1);
 		
@@ -111,7 +111,7 @@ public class CacheLogAnalyzer {
 		}
 		
 		incStats("access", "hot_lfns", lfn, hit);
-		System.out.println("Incrementing lfn "+lfn+" with "+hit);
+		//System.out.println("Incrementing lfn "+lfn+" with "+hit);
 		
 		if (lfn.startsWith("/alice/data/")){
 			if (lfn.indexOf("/OCDB/")>=0){
