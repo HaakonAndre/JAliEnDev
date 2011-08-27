19e098d1ee71059b9909661f430419e9b3896d65
costing <costing@ac58307a-7d81-4c66-b533-6213255c5d0c>
2011-08-27 11:48:04 +0000

    lock one more table

--- src/alien/io/TransferBroker.java
+++ src/alien/io/TransferBroker.java
@@ -116,7 +116,7 @@ public class TransferBroker {
 		
 		final DBConnection dbc = db.getConnection();
 		
-		executeQuery(dbc, "lock tables TRANSFERS_DIRECT write;");
+		executeQuery(dbc, "lock tables TRANSFERS_DIRECT write, PROTOCOLS write;");
 		executeQuery(dbc, "select transferId,lfn,destination from TRANSFERS_DIRECT inner join PROTOCOLS on (sename=destination) where status='WAITING' and current_transfers<max_transfers order by transferId asc limit 1;");
 	
 		int transferId = -1;
