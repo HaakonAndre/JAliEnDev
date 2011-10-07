86f599f9dc0518d552303c03c48018c306cb7d9d
steffen <steffen@ac58307a-7d81-4c66-b533-6213255c5d0c>
2011-10-07 14:57:30 +0000

    caller and runner for the test system

---
+++ test.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+#export PATH="$PATH:$HOME/alien/bin:$HOME/alien/api/bin"
+#export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:$HOME/alien/lib:$HOME/alien/api/lib"
+
+CLASSPATH=bin:build_eclipse
+
+for jar in lib/*.jar; do
+    CLASSPATH="$CLASSPATH:$jar"
+done
+
+CLASSPATH="$CLASSPATH:alien.jar"
+
+
+export CLASSPATH
+
+java -Duserid=$(id -u) -XX:CompileThreshold=5 -Dsun.security.ssl.allowUnsafeRenegotiation=true  "$@"
+#java -XX:CompileThreshold=5   "$@"
+
