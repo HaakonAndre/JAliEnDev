82a42df2a0edca30f844026e678bb3aa5d3b6d5d
costing <costing@ac58307a-7d81-4c66-b533-6213255c5d0c>
2011-11-03 14:37:13 +0000

    minor cleanup

--- src/alien/shell/commands/JAliEnCommandps.java
+++ src/alien/shell/commands/JAliEnCommandps.java
@@ -2,7 +2,6 @@ package alien.shell.commands;
 
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.EnumSet;
 import java.util.List;
 import java.util.Set;
 import java.util.StringTokenizer;
