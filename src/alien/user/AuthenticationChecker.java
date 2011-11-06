b9b3b0ecb186b36c66c2e659d80b1eff8e459a48
costing <costing@ac58307a-7d81-4c66-b533-6213255c5d0c>
2011-11-06 20:38:36 +0000

    make challenge instance-local instead of static ... is this ok ?

--- src/alien/user/AuthenticationChecker.java
+++ src/alien/user/AuthenticationChecker.java
@@ -9,7 +9,6 @@ import java.security.KeyPair;
 import java.security.NoSuchAlgorithmException;
 import java.security.Signature;
 import java.security.SignatureException;
-import java.security.cert.Certificate;
 import java.security.cert.X509Certificate;
 import java.security.interfaces.RSAPrivateKey;
 import java.security.interfaces.RSAPublicKey;
@@ -31,65 +30,89 @@ public class AuthenticationChecker {
 	private static RSAPrivateKey privKey = null;
 	private static RSAPublicKey pubKey = null;
 	private static X509Certificate cert = null;
-	
-	private static String challenge = null;
 
-	
-	static{
-		privKey = null; // (RSAPrivateKey) JAKeyStore.ks.getKey("User.cert", JAKeyStore.pass);		
-			
-		Certificate[] usercert = null; //JAKeyStore.ks.getCertificateChain("User.cert");
-		pubKey = null; //(RSAPublicKey) usercert[0].getPublicKey();
+	private String challenge = null;
+
+	static {
+		privKey = null; // (RSAPrivateKey) JAKeyStore.ks.getKey("User.cert", JAKeyStore.pass);
+
+		//Certificate[] usercert = null; // JAKeyStore.ks.getCertificateChain("User.cert");
+		pubKey = null; // (RSAPublicKey) usercert[0].getPublicKey();
 	}
-	
 
 	/**
 	 * @param pFinder
 	 * @throws IOException
 	 */
-	public static  void loadPrivKey(PasswordFinder pFinder) throws IOException {
+	public static void loadPrivKey(PasswordFinder pFinder) throws IOException {
 
-		if(privKey==null){
-			BufferedReader priv = new BufferedReader(new FileReader(ConfigUtils
-					.getConfig().gets("user.cert.priv.location").trim()));
+		if (privKey == null) {
+			BufferedReader priv = null;
 
-			if(pFinder.getPassword().length==0)
-				privKey = (RSAPrivateKey) ((KeyPair) new PEMReader(priv)
-					.readObject()).getPrivate();
-			else
-				privKey = (RSAPrivateKey) ((KeyPair) new PEMReader(priv, pFinder)
-				.readObject()).getPrivate();
+			PEMReader reader = null;
+
+			try {
+				priv = new BufferedReader(new FileReader(ConfigUtils.getConfig().gets("user.cert.priv.location").trim()));
+
+				if (pFinder.getPassword().length == 0)
+					reader = new PEMReader(priv);
+				else
+					reader = new PEMReader(priv, pFinder);
+
+				privKey = (RSAPrivateKey) ((KeyPair) reader.readObject()).getPrivate();
+			}
+			finally {
+				try {
+					if (reader != null) {
+						reader.close();
+					}
+				}
+				catch (IOException ioe) {
+					// ignore
+				}
+
+				try {
+					if (priv != null)
+						priv.close();
+				}
+				catch (IOException ioe) {
+					// ignore
+				}
+			}
 		}
 	}
 
 	/**
 	 * @param pubCert
 	 */
-	public static  void loadPubCert(String pubCert) {
+	public static void loadPubCert(String pubCert) {
 
 		try {
 			StringReader pub = new StringReader(pubCert);
-			 cert = ((X509Certificate) new PEMReader(pub).readObject());
-			
+			cert = ((X509Certificate) new PEMReader(pub).readObject());
+
 			pubKey = (RSAPublicKey) cert.getPublicKey();
-		} catch (IOException e) {
+		}
+		catch (IOException e) {
 			e.printStackTrace();
 		}
 	}
 
 	/**
 	 * Read public user certificate from config location
+	 * 
 	 * @return the public certificate
 	 * @throws IOException
 	 */
-	public static  String readPubCert() throws IOException {
+	public static String readPubCert() throws IOException {
 		String location = ConfigUtils.getConfig().gets("user.cert.pub.location").trim();
-		
+
 		return Utils.readFile(location);
 	}
 
 	/**
 	 * Create a challenge
+	 * 
 	 * @return the challenge
 	 */
 	public String challenge() {
@@ -99,15 +122,14 @@ public class AuthenticationChecker {
 
 	/**
 	 * sign the challenge as a response
+	 * 
 	 * @param challengeText
 	 * @return the response
 	 * @throws SignatureException
 	 * @throws NoSuchAlgorithmException
 	 * @throws InvalidKeyException
 	 */
-	public String response(String challengeText)
-			throws SignatureException, NoSuchAlgorithmException,
-			InvalidKeyException {
+	public static String response(final String challengeText) throws SignatureException, NoSuchAlgorithmException, InvalidKeyException {
 		Signature signature = Signature.getInstance("SHA384withRSA");
 		signature.initSign(privKey);
 		signature.update(challengeText.getBytes());
@@ -117,6 +139,7 @@ public class AuthenticationChecker {
 
 	/**
 	 * verify the response as signature
+	 * 
 	 * @param pubCertString
 	 * @param signature
 	 * @return verified or not
@@ -124,9 +147,7 @@ public class AuthenticationChecker {
 	 * @throws NoSuchAlgorithmException
 	 * @throws InvalidKeyException
 	 */
-	public boolean verify(String pubCertString, String signature)
-			throws SignatureException, NoSuchAlgorithmException,
-			InvalidKeyException {
+	public boolean verify(String pubCertString, String signature) throws SignatureException, NoSuchAlgorithmException, InvalidKeyException {
 
 		loadPubCert(pubCertString);
 		Signature verifier = Signature.getInstance("SHA384withRSA");
@@ -136,7 +157,7 @@ public class AuthenticationChecker {
 			System.out.println("Access granted for user: " + cert.getSubjectDN());
 			return true;
 		}
-		
+
 		System.out.println("Access denied");
 		return false;
 	}
