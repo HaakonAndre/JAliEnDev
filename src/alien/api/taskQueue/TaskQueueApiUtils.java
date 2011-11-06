2ec8a50292c088e369d4436615a5e6fa15886db2
costing <costing@ac58307a-7d81-4c66-b533-6213255c5d0c>
2011-11-06 22:00:36 +0000

    reformatting

--- src/alien/api/taskQueue/TaskQueueApiUtils.java
+++ src/alien/api/taskQueue/TaskQueueApiUtils.java
@@ -1,106 +1,105 @@
-						package alien.api.taskQueue;
-						
-						import java.io.IOException;
+package alien.api.taskQueue;
+
+import java.io.IOException;
 import java.util.Collection;
-						import java.util.List;
-						import java.util.Map;
-						import java.util.Set;
-						
-						import alien.api.Dispatcher;
-						import alien.shell.commands.JAliEnCOMMander;
-						import alien.taskQueue.JDL;
-						import alien.taskQueue.Job;
-						import alien.taskQueue.JobSigner;
-						import alien.taskQueue.JobStatus;
-						import alien.taskQueue.JobSubmissionException;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+import alien.api.Dispatcher;
+import alien.shell.commands.JAliEnCOMMander;
+import alien.taskQueue.JDL;
+import alien.taskQueue.Job;
+import alien.taskQueue.JobSigner;
+import alien.taskQueue.JobStatus;
+import alien.taskQueue.JobSubmissionException;
 import alien.user.JAKeyStore;
-						
-						/**
-						 * Get the JDL object
-						 * 
-						 * @author ron
-						 * @since Jun 05, 2011
-						 */
-						public class TaskQueueApiUtils {
-							
-							private final JAliEnCOMMander commander;
-							
-							
-							/**
-							 * @param commander
-							 */
-							public TaskQueueApiUtils(JAliEnCOMMander commander){
-								this.commander = commander;
+
+/**
+ * Get the JDL object
+ * 
+ * @author ron
+ * @since Jun 05, 2011
+ */
+public class TaskQueueApiUtils {
+
+	private final JAliEnCOMMander commander;
+
+	/**
+	 * @param commander
+	 */
+	public TaskQueueApiUtils(JAliEnCOMMander commander) {
+		this.commander = commander;
 	}
 
 	/**
 	 * @return the uptime / w statistics
 	 */
-	public static Map<String, GetUptime.UserStats> getUptime(){
-		try{
+	public static Map<String, GetUptime.UserStats> getUptime() {
+		try {
 			final GetUptime uptime = (GetUptime) Dispatcher.execute(new GetUptime(), false);
-			
+
 			return uptime.getStats();
 		}
-		catch (IOException ioe){
-			System.out.println("Could not get an uptime stats: "+ioe.getMessage());
+		catch (IOException ioe) {
+			System.out.println("Could not get an uptime stats: " + ioe.getMessage());
 			ioe.printStackTrace();
 		}
-		
+
 		return null;
 	}
-	
+
 	/**
-	 * @param states 
-	 * @param users 
-	 * @param sites 
-	 * @param nodes 
-	 * @param mjobs 
-	 * @param jobid 
-	 * @param orderByKey 
-	 * @param limit 
+	 * @param states
+	 * @param users
+	 * @param sites
+	 * @param nodes
+	 * @param mjobs
+	 * @param jobid
+	 * @param orderByKey
+	 * @param limit
 	 * @return a PS listing
 	 */
-	public List<Job> getPS(final Collection<JobStatus> states,final Collection<String> users,final Collection<String> sites,
-			final Collection<String> nodes,final Collection<Integer> mjobs,final Collection<Integer> jobid, final String orderByKey, final int limit) {
+	public List<Job> getPS(final Collection<JobStatus> states, final Collection<String> users, final Collection<String> sites, final Collection<String> nodes, final Collection<Integer> mjobs,
+			final Collection<Integer> jobid, final String orderByKey, final int limit) {
 
 		try {
 			GetPS ps = (GetPS) Dispatcher.execute(new GetPS(commander.getUser(), commander.getRole(), states, users, sites, nodes, mjobs, jobid, orderByKey, limit), true);
 
 			return ps.returnPS();
-		} catch (IOException e) {
-			System.out.println("Could not get a PS listing: "+e.getMessage());
+		}
+		catch (IOException e) {
+			System.out.println("Could not get a PS listing: " + e.getMessage());
 			e.printStackTrace();
 		}
 		return null;
 	}
-	
 
-	
 	/**
-	 * @param status 
-	 * @param id 
-	 * @param jobId 
-	 * @param site 
-	 * @param bPrintId 
-	 * @param bPrintSite 
-	 * @param bMerge 
-	 * @param bKill 
-	 * @param bResubmit 
-	 * @param bExpunge 
+	 * @param status
+	 * @param id
+	 * @param jobId
+	 * @param site
+	 * @param bPrintId
+	 * @param bPrintSite
+	 * @param bMerge
+	 * @param bKill
+	 * @param bResubmit
+	 * @param bExpunge
 	 * @return a PS listing
 	 */
-	public List<Job>  getMasterJobStatus( final int jobId, final Set<JobStatus> status, final List<Integer> id, final List<String> site, final boolean bPrintId,
-			final boolean bPrintSite, final boolean bMerge, final boolean bKill, final boolean bResubmit, final boolean bExpunge) {
+	public List<Job> getMasterJobStatus(final int jobId, final Set<JobStatus> status, final List<Integer> id, final List<String> site, final boolean bPrintId, final boolean bPrintSite,
+			final boolean bMerge, final boolean bKill, final boolean bResubmit, final boolean bExpunge) {
 
 		try {
-			GetMasterjob mj = (GetMasterjob) Dispatcher.execute(new GetMasterjob(commander.getUser(), commander.getRole(),
-					jobId, status, id, site, bPrintId, bPrintSite, bMerge, bKill, bResubmit, bExpunge), true);
-						
-//			return mj.masterJobStatus();
+			GetMasterjob mj = (GetMasterjob) Dispatcher.execute(new GetMasterjob(commander.getUser(), commander.getRole(), jobId, status, id, site, bPrintId, bPrintSite, bMerge, bKill, bResubmit,
+					bExpunge), true);
+
+			// return mj.masterJobStatus();
 			return mj.subJobStatus();
-			
-		} catch (IOException e) {
+
+		}
+		catch (IOException e) {
 			System.out.println("Could get a PS listing: ");
 			e.printStackTrace();
 		}
@@ -108,10 +107,8 @@ import alien.user.JAKeyStore;
 
 	}
 
-
-	
 	/**
-	 * @param queueId 
+	 * @param queueId
 	 * @return a JDL as String
 	 */
 	public String getTraceLog(final int queueId) {
@@ -120,16 +117,17 @@ import alien.user.JAKeyStore;
 			GetTraceLog trace = (GetTraceLog) Dispatcher.execute(new GetTraceLog(commander.getUser(), commander.getRole(), queueId), true);
 
 			return trace.getTraceLog();
-		} catch (IOException e) {
+		}
+		catch (IOException e) {
 			System.out.println("Could get not a TraceLog: ");
 			e.printStackTrace();
 		}
 		return null;
 
 	}
-	
+
 	/**
-	 * @param queueId 
+	 * @param queueId
 	 * @return a JDL as String
 	 */
 	public String getJDL(final int queueId) {
@@ -138,16 +136,17 @@ import alien.user.JAKeyStore;
 			GetJDL jdl = (GetJDL) Dispatcher.execute(new GetJDL(commander.getUser(), commander.getRole(), queueId), true);
 
 			return jdl.getJDL();
-		} catch (IOException e) {
+		}
+		catch (IOException e) {
 			System.out.println("Could get not a JDL: ");
 			e.printStackTrace();
 		}
 		return null;
 
 	}
-	
+
 	/**
-	 * @param queueId 
+	 * @param queueId
 	 * @return a Job
 	 */
 	public Job getJob(final int queueId) {
@@ -156,14 +155,15 @@ import alien.user.JAKeyStore;
 			GetJob job = (GetJob) Dispatcher.execute(new GetJob(commander.getUser(), commander.getRole(), queueId), true);
 
 			return job.getJob();
-		} catch (IOException e) {
+		}
+		catch (IOException e) {
 			System.out.println("Could get not the Job: ");
 			e.printStackTrace();
 		}
 		return null;
 
 	}
-	
+
 	/**
 	 * @param queueIds
 	 * @return a Job
@@ -174,14 +174,15 @@ import alien.user.JAKeyStore;
 			GetJobs job = (GetJobs) Dispatcher.execute(new GetJobs(commander.getUser(), commander.getRole(), queueIds), true);
 
 			return job.getJobs();
-		} catch (IOException e) {
+		}
+		catch (IOException e) {
 			System.out.println("Could get not the Jobs: ");
 			e.printStackTrace();
 		}
 		return null;
 
 	}
-	
+
 	/**
 	 * Set a job's status
 	 * 
@@ -192,7 +193,8 @@ import alien.user.JAKeyStore;
 		try {
 			Dispatcher.execute(new SetJobStatus(jobnumber, status), true);
 
-		} catch (IOException e) {
+		}
+		catch (IOException e) {
 			System.out.println("Could get not a Job's status: ");
 			e.printStackTrace();
 		}
@@ -210,35 +212,44 @@ import alien.user.JAKeyStore;
 		try {
 			JDL ojdl = new JDL(jdl);
 			SubmitJob j;
-			j = new SubmitJob(commander.getUser(), commander.getRole(), JobSigner.signJob(JAKeyStore.clientCert,
-					"User.cert", JAKeyStore.pass, commander.getUser().getName(), ojdl, jdl));	// TODO : why was ojdl passed here ?
+			j = new SubmitJob(commander.getUser(), commander.getRole(), JobSigner.signJob(JAKeyStore.clientCert, "User.cert", JAKeyStore.pass, commander.getUser().getName(), ojdl, jdl)); // TODO
+																																															// :
+																																															// why
+																																															// was
+																																															// ojdl
+																																															// passed
+																																															// here
+																																															// ?
 
 			Dispatcher.execute(j, true);
 			return j.getJobID();
 
-		} catch (Exception e) {
+		}
+		catch (Exception e) {
 			System.out.println("Could not submit a JDL: ");
 			e.printStackTrace();
 		}
 		return 0;
 	}
-	
+
 	/**
 	 * Kill a job
-	 * @param queueId 
 	 * 
-	 * @return status of the kill 
+	 * @param queueId
+	 * 
+	 * @return status of the kill
 	 */
-	public boolean killJob(int queueId){
+	public boolean killJob(int queueId) {
 
 		try {
-			KillJob j = new KillJob(commander.getUser(), commander.getRole(),queueId);
+			KillJob j = new KillJob(commander.getUser(), commander.getRole(), queueId);
 
 			Dispatcher.execute(j, true);
 			return j.wasKilled();
 
-		} catch (Exception e) {
-			System.out.println("Could not kill the job  with id: [" + queueId+ "]");
+		}
+		catch (Exception e) {
+			System.out.println("Could not kill the job  with id: [" + queueId + "]");
 			e.printStackTrace();
 		}
 		return false;
