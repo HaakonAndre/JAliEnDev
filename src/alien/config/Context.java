f4069be6040e61212d7c2e468936613e779f82fc
costing <costing@ac58307a-7d81-4c66-b533-6213255c5d0c>
2011-11-06 23:10:14 +0000

    simplify the code quite a bit

--- src/alien/config/Context.java
+++ src/alien/config/Context.java
@@ -1,10 +1,7 @@
 package alien.config;
 
 import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Iterator;
 import java.util.Map;
-import java.util.Set;
 import java.util.concurrent.ConcurrentHashMap;
 
 /**
@@ -15,7 +12,7 @@ public class Context {
 	/**
 	 * Thread-tied contexts
 	 */
-	static Map<Long, Map<String, Object>> context = new ConcurrentHashMap<Long, Map<String, Object>>();
+	static Map<Thread, Map<String, Object>> context = new ConcurrentHashMap<Thread, Map<String, Object>>();
 	
 	static {
 		final Thread cleanupThread = new Thread("alien.config.Context.cleanup"){
@@ -25,20 +22,7 @@ public class Context {
 					try{
 						Thread.sleep(1000*60);
 						
-						final Set<Long> threadIDs = new HashSet<Long>();
-						
-						for (final Thread t: Thread.getAllStackTraces().keySet()){
-							threadIDs.add(Long.valueOf(t.getId()));
-						}
-						
-						final Iterator<Map.Entry<Long, Map<String, Object>>> it = context.entrySet().iterator();
-						
-						while (it.hasNext()){
-							final Map.Entry<Long, Map<String, Object>> me = it.next();
-							
-							if (!threadIDs.contains(me.getKey()))
-								it.remove();
-						}
+						context.keySet().retainAll(Thread.getAllStackTraces().keySet());
 					}
 					catch (final Throwable t){
 						// ignore
@@ -57,14 +41,12 @@ public class Context {
 	 * @return the previously set value for this key
 	 */
 	public static Object setThreadContext(final String key, final Object value){
-		final Long threadID = Long.valueOf(Thread.currentThread().getId());
-		
-		Map<String, Object> m = context.get(threadID);
+		Map<String, Object> m = context.get(Thread.currentThread());
 		
 		if (m==null){
 			// the map will be accessed from within the same thread, so there can be no conflict here
 			m = new HashMap<String, Object>();
-			context.put(threadID, m);
+			context.put(Thread.currentThread(), m);
 		}
 		
 		return m.put(key, value);
@@ -75,7 +57,7 @@ public class Context {
 	 * @return the value associated with this key for the current thread
 	 */
 	public static Object getTheadContext(final String key){
-		final Map<String, Object> m = context.get(Long.valueOf(Thread.currentThread().getId()));
+		final Map<String, Object> m = context.get(Thread.currentThread());
 		
 		if (m==null)
 			return null;
