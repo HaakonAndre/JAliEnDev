c7e56cc0d4d9df6e3481b8b1bb78dfccd5ec65fc
steffen <steffen@ac58307a-7d81-4c66-b533-6213255c5d0c>
2011-11-23 16:37:34 +0000

    cleaning

--- src/alien/catalogue/LFN.java
+++ src/alien/catalogue/LFN.java
@@ -9,7 +9,6 @@ import java.util.List;
 import java.util.Set;
 import java.util.TreeSet;
 import java.util.UUID;
-import java.util.logging.Level;
 import java.util.logging.Logger;
 
 import lazyj.DBFunctions;
