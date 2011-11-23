0655a50c01f516cf8835e1521b9f712f5f4e0d46
steffen <steffen@ac58307a-7d81-4c66-b533-6213255c5d0c>
2011-11-23 12:46:16 +0000

    simplifying the starter

--- test.sh
+++ test.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-#export PATH="$PATH:$HOME/alien/bin:$HOME/alien/api/bin"
-#export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:$HOME/alien/lib:$HOME/alien/api/lib"
-
 CLASSPATH=bin:build_eclipse
 
 for jar in lib/*.jar; do
@@ -13,6 +10,5 @@ CLASSPATH="$CLASSPATH:alien.jar"
 
 export CLASSPATH
 
-java -Duserid=$(id -u) -DAliEnConfig=$HOME/.alien/testVO/config -XX:CompileThreshold=5 -Dsun.security.ssl.allowUnsafeRenegotiation=true  "$@" $(pwd) 
-#java -XX:CompileThreshold=5   "$@"
+java -Duserid=$(id -u) -DAliEnConfig=$HOME/.j/testVO/config "$@" $(pwd) 
 
