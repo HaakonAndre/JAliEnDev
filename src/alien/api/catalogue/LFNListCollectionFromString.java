d800c3a1baf65091aee6a8fe14b1ab7e66e90ea3
costing <costing@ac58307a-7d81-4c66-b533-6213255c5d0c>
2012-01-11 15:43:46 +0000

    list files from collection

---
+++ src/alien/api/catalogue/LFNListCollectionFromString.java
@@ -0,0 +1,70 @@
+package alien.api.catalogue;
+
+import java.util.LinkedHashSet;
+import java.util.Set;
+
+import alien.api.Request;
+import alien.catalogue.LFN;
+import alien.catalogue.LFNUtils;
+import alien.user.AliEnPrincipal;
+
+/**
+ * Get the LFN object for this path
+ * 
+ * @author ron
+ * @since Jun 08, 2011
+ */
+public class LFNListCollectionFromString extends Request {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -7167353294190733455L;
+	private final String path;
+
+	private Set<LFN> lfns = null;
+
+	/**
+	 * @param user 
+	 * @param role 
+	 * @param path
+	 */
+	public LFNListCollectionFromString(final AliEnPrincipal user, final String role, final String path) {
+		setRequestUser(user);
+		setRoleRequest(role);
+		this.path = path;
+	}
+
+	@Override
+	public void run() {
+		final LFN entry = LFNUtils.getLFN(path, false);
+
+		if (entry != null) {
+			if (entry.isCollection()){
+				final Set<String> entries = entry.listCollection();
+				
+				this.lfns = new LinkedHashSet<LFN>(entries.size());
+				
+				for (final String lfn: entries){
+					this.lfns.add(LFNUtils.getLFN(lfn));
+				}
+			}
+			else
+				throw new IllegalArgumentException("Not a collection");
+		}
+		
+		throw new IllegalArgumentException("No such LFN");
+	}
+
+	/**
+	 * @return the requested LFN
+	 */
+	public Set<LFN> getLFNs() {
+		return this.lfns;
+	}
+
+	@Override
+	public String toString() {
+		return "Asked for : " + this.path + " reply is: " + this.lfns;
+	}
+}
--- src/alien/shell/commands/JAliEnCommandlistFilesFromCollection.java
+++ src/alien/shell/commands/JAliEnCommandlistFilesFromCollection.java
@@ -1,11 +1,14 @@
 package alien.shell.commands;
 
 import java.util.ArrayList;
-import java.util.List;
+import java.util.Set;
 
 import joptsimple.OptionException;
 import joptsimple.OptionParser;
 import joptsimple.OptionSet;
+import alien.api.Dispatcher;
+import alien.api.ServerException;
+import alien.api.catalogue.LFNListCollectionFromString;
 import alien.catalogue.FileSystemUtils;
 import alien.catalogue.LFN;
 
@@ -20,38 +23,35 @@ public class JAliEnCommandlistFilesFromCollection extends JAliEnBaseCommand {
 	/**
 	 * the LFN for path
 	 */
-	private List<LFN> lfns = new ArrayList<LFN>();
+	private Set<LFN> lfns = null;
 
+	private String errorMessage = null;
+	
 	/**
 	 * execute the type
 	 */
 	@Override
-	public void run() {
+	public void run() {		
+		final String path = FileSystemUtils.getAbsolutePath(commander.user.getName(), commander.getCurrentDir().getCanonicalName(), sPath);
 		
-		LFN coll = null;
-
-		if (sPath != null)
-			coll = commander.c_api.getLFN(FileSystemUtils.getAbsolutePath(
-					commander.user.getName(), commander.getCurrentDir()
-							.getCanonicalName(), sPath));
-
-
-		if (coll == null){
+		try{
+			final LFNListCollectionFromString ret = Dispatcher.execute(new LFNListCollectionFromString(commander.getUser(), commander.getRole(), path), true);
+			
+			lfns = ret.getLFNs();
+		}
+		catch (ServerException e){
+			Throwable cause = e.getCause();
+			
+			errorMessage = cause.getMessage();
+		}
+		
+		if (errorMessage != null){
 			if(!isSilent())
-				out.printErrln("No such file or directory: [" + sPath + "]");
+				out.printErrln(errorMessage);
+			
 			return;
 		}
 		
-		if(!coll.isCollection()){
-			if(!isSilent())
-				out.printOutln("Not a collection: [" + sPath + "]");
-			return;
-		}
-		
-
-		for(String c: coll.listCollection())
-			lfns.add(commander.c_api.getLFN(c));
-				
 		//if (out.isRootPrinter())
 			out.setReturnArgs(deserializeForRoot());
 	}
