1a3c92cba41984131775c4257250cec618c0cc5b
costing <costing@ac58307a-7d81-4c66-b533-6213255c5d0c>
2012-03-02 14:32:24 +0000

    do not delete yet

--- src/utils/OrphanPFNsCleanup.java
+++ src/utils/OrphanPFNsCleanup.java
@@ -105,7 +105,8 @@ public class OrphanPFNsCleanup {
 				concurrentQueryies.acquireUninterruptibly();
 				
 				try{
-					db.query("DELETE FROM orphan_pfns WHERE se="+seNumber+" AND fail_count>10;");
+					// TODO : what to do with these PFNs ? Iterate over them and release them from the catalogue nevertheless ?
+//					db.query("DELETE FROM orphan_pfns WHERE se="+seNumber+" AND fail_count>10;");
 					
 					db.query("SELECT binary2string(guid) FROM orphan_pfns WHERE se="+seNumber+" ORDER BY fail_count ASC LIMIT 10000;");
 				}
