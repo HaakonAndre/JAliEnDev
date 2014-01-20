5d5b22da9c39eba9403ea92ef4f26abb94222258
costing <costing@ac58307a-7d81-4c66-b533-6213255c5d0c>
2014-01-20 10:33:35 +0000

    Perl variable output

--- src/alien/servlets/TokenGenerator.java
+++ src/alien/servlets/TokenGenerator.java
@@ -3,6 +3,7 @@ package alien.servlets;
 import java.io.IOException;
 
 import javax.servlet.ServletException;
+import javax.servlet.ServletOutputStream;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -24,6 +25,14 @@ public class TokenGenerator extends HttpServlet {
 
 	@Override
 	protected void doGet(final HttpServletRequest req, final HttpServletResponse resp) throws ServletException, IOException {
-		resp.getOutputStream().println(JobToken.generateToken());
+		final ServletOutputStream os = resp.getOutputStream();
+		
+		os.println("$VAR1 = [");
+		os.println("  {");
+		os.println("    'token' => '"+JobToken.generateToken()+"'");
+		os.println("  }");
+		os.println("];");
+		
+		os.close();
 	}
 }
