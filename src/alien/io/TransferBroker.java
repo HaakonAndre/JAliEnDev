1365e60d5c92a5d160909c62dff3ae3061509f12
costing <costing@ac58307a-7d81-4c66-b533-6213255c5d0c>
2014-05-04 17:19:17 +0000

    increase advanced reading cache size

--- src/alien/io/TransferBroker.java
+++ src/alien/io/TransferBroker.java
@@ -156,7 +156,7 @@ public class TransferBroker {
 		try {
 			while (transferId < 0){
 				if (!dbCached.moveNext()){
-					dbCached.query("select transferId,lfn,destination,remove_replica from TRANSFERS_DIRECT inner join PROTOCOLS on (sename=destination) where status='WAITING' and (SELECT count(1) FROM active_transfers WHERE se_name=sename)<max_transfers and attempts>=0 order by attempts desc,transferId asc limit 100;");
+					dbCached.query("select transferId,lfn,destination,remove_replica from TRANSFERS_DIRECT inner join PROTOCOLS on (sename=destination) where status='WAITING' and (SELECT count(1) FROM active_transfers WHERE se_name=sename)<max_transfers and attempts>=0 order by attempts desc,transferId asc limit 1000;");
 
 					if (!dbCached.moveNext()){
 						logger.log(Level.FINE, "There is no waiting transfer in the queue");
