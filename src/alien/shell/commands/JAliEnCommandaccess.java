43be4b142e9937895c97de81d4962a4b0109e1a6
agrigora <alina.gabriela.grigoras@cern.ch>
2014-08-05 16:40:19 +0200

    Sending all envelops to ROOT

--- src/alien/shell/commands/JAliEnCommandaccess.java
+++ src/alien/shell/commands/JAliEnCommandaccess.java
@@ -39,14 +39,14 @@ public class JAliEnCommandaccess extends JAliEnBaseCommand {
 
 	private List<String> ses = new ArrayList<>();
 	private List<String> exses = new ArrayList<>();
-	
+
 	private HashMap<String,Integer> qos = new HashMap<>();
 
 	/**
 	 * name of a local File that will be written
 	 */
 	private String localFileName = null;
-	
+
 
 	/**
 	 * return pfns;
@@ -58,82 +58,87 @@ public class JAliEnCommandaccess extends JAliEnBaseCommand {
 	 */
 	@Override
 	public void run() {
-		
+
 		LFN lfn = null;
 		GUID guid = null;
 		boolean evenIfNotExists = false;
 
-		if (accessRequest.equals(AccessType.WRITE))
+		if (accessRequest.equals(AccessType.WRITE)){
+			logger.log(Level.INFO, "Access called for a write operation");
 			evenIfNotExists = true;
+		}
 
-			lfn = commander.c_api.getLFN(lfnName, evenIfNotExists);
-			if (lfn == null) {
-				out.printErrln("Not able to retrieve LFN from Catalogue [error in processing].");
+		//obtaining LFN information for read or a new LFN for write
+		lfn = commander.c_api.getLFN(lfnName, evenIfNotExists);
+		
+		if (lfn == null) {
+			logger.log(Level.INFO, "Not able to retrieve LFN from Catalogue ");
+			out.printErrln("Not able to retrieve LFN from Catalogue [error in processing].");
+			return;
+		}
+		
+		//is it ok here? to check, is this for a new file
+		if (lfn.guid == null){
+			try {
+				guid = GUIDUtils.createGuid(new File(localFileName),commander.user);
+			} catch (IOException e) {
+				e.printStackTrace();
+				//TODO
 				return;
 			}
-			if (lfn.guid == null) 
-			{
-				
-					try {
-						guid = GUIDUtils.createGuid(new File(localFileName),commander.user);
-					} catch (IOException e) {
-						e.printStackTrace();
-						//TODO
-						return;
-					}
-					lfn.guid = guid.guid;
-					lfn.size = guid.size;
-					lfn.md5 = guid.md5;
-					guid.lfnCache = new LinkedHashSet<>(1);
-					guid.lfnCache.add(lfn);
-			}
-				
-		
+			
+			lfn.guid = guid.guid;
+			lfn.size = guid.size;
+			lfn.md5 = guid.md5;
+			guid.lfnCache = new LinkedHashSet<>(1);
+			guid.lfnCache.add(lfn);
+		}
 
 		if (accessRequest == AccessType.WRITE) 
-				pfns = commander.c_api.getPFNsToWrite(lfn, guid, ses, exses, qos);
-		
+			pfns = commander.c_api.getPFNsToWrite(lfn, guid, ses, exses, qos);
 		else if (accessRequest == AccessType.READ) {
-			logger.log(Level.INFO, "We are doing a read operation");	
+			logger.log(Level.INFO, "Acess called for a read operation");	
 			pfns = commander.c_api.getPFNsToRead(lfn, ses, exses);
-		
-		}else
-			
+		}
+		else{
+			logger.log(Level.SEVERE, "Unknown access type");
 			out.printErrln("Unknown access type [error in processing].");
-
-		if(pfns==null || pfns.size()<1)
+		}
+		
+		if(pfns == null || pfns.size() < 1){
+			logger.log(Level.SEVERE, "Error getting the LFN/GUID");
 			out.printErrln("Not able to get request LFN/GUID [error in processing].");
+		}
+
+		if (out.isRootPrinter()){
 		
-		
-		if (out.isRootPrinter())
-		{
-			out.nextResult();
-			if (pfns != null && !pfns.isEmpty())
-			{
-				for (PFN pfn : pfns) 
-				{
+			if (pfns != null && !pfns.isEmpty()){
+				for (PFN pfn : pfns){
+					out.nextResult();
+					
 					String envelope = pfn.ticket.envelope.getSignedEnvelope();
+					
 					if (!"alice::cern::setest".equals(commander.c_api.getSE(pfn.seNumber)
 							.getName().toLowerCase()))
 						if (commander.c_api.getSE(pfn.seNumber).needsEncryptedEnvelope)
 							envelope += "&envelope="
 									+ pfn.ticket.envelope.getEncryptedEnvelope();
-					
+
 					final StringTokenizer st = new StringTokenizer(envelope, "&");
-					while (st.hasMoreTokens()) 
-					{
+					
+					while (st.hasMoreTokens()) {
 						String t = st.nextToken();
 						String key = t.substring(0, t.indexOf('='));
 						String val = t.substring(t.indexOf('=') + 1);
-						if (("turl").equals(key)) 
-						{
+						
+						if (("turl").equals(key)) {
 							out.setField("url", val);
 							final StringTokenizer tpfn = new StringTokenizer(val,
 									"////");
 							tpfn.nextToken();
 							tpfn.nextToken();
 							StringBuilder ttpfn = new StringBuilder();
-							
+
 							while (tpfn.hasMoreTokens()){
 								ttpfn.append('/').append(tpfn.nextToken());
 							}
@@ -145,7 +150,7 @@ public class JAliEnCommandaccess extends JAliEnBaseCommand {
 					if (accessRequest.equals(AccessType.WRITE))
 						out.setField("nSEs", "1");
 					else 
-						
+
 						out.setField("nSEs", " "+pfns.size());
 					out.setField("user", commander.user.getName());
 					if (accessRequest.equals(AccessType.WRITE))
@@ -178,73 +183,7 @@ public class JAliEnCommandaccess extends JAliEnBaseCommand {
 	 * 
 	 * @return serialized return
 	 */
-	
-	@Override
-	public String deserializeForRoot() {
 
-		String ret = "";
-		if (pfns != null && !pfns.isEmpty()) {
-
-			String col = RootPrintWriter.columnseparator;
-			String desc = RootPrintWriter.fielddescriptor;
-			String sep = RootPrintWriter.fieldseparator;
-
-			//<stderrindicator>\n<columnseparator><fieldseparator><outputindicator>\n<columnseparator><fielddescriptor>eof<fieldseparator>1<outputterminator>\n<columnseparator><fielddescriptor>pwd<fieldseparator>/alice/cern.ch/user/s/sschrein/edittest/<streamend>
-			
-			for (PFN pfn : pfns) {
-				ret += col;
-
-				String envelope = pfn.ticket.envelope.getSignedEnvelope();
-				if (!"alice::cern::setest".equals(commander.c_api.getSE(pfn.seNumber)
-						.getName().toLowerCase()))
-					if (commander.c_api.getSE(pfn.seNumber).needsEncryptedEnvelope)
-						envelope += "&envelope="
-								+ pfn.ticket.envelope.getEncryptedEnvelope();
-				
-				final StringTokenizer st = new StringTokenizer(envelope, "&");
-				while (st.hasMoreTokens()) 
-				{
-					String t = st.nextToken();
-					String key = t.substring(0, t.indexOf('='));
-					String val = t.substring(t.indexOf('=') + 1);
-
-					if (("turl").equals(key)) 
-					{
-						ret += desc + "url" + sep + val;
-						final StringTokenizer tpfn = new StringTokenizer(val,
-								"////");
-						tpfn.nextToken();
-						tpfn.nextToken();
-						StringBuilder ttpfn = new StringBuilder();
-						
-						while (tpfn.hasMoreTokens()){
-							ttpfn.append('/').append(tpfn.nextToken());
-						}
-
-						ret += desc + "pfn" + sep + ttpfn.toString();
-					}
-					else 
-						ret += desc + key + sep + val;
-
-				}
-				
-				if (accessRequest.equals(AccessType.WRITE))
-					ret += desc + "nSEs" + sep + "1";
-				else 
-					ret += desc + "nSEs" + sep + pfns.size();
-				
-				ret += desc + "user" + sep + commander.user.getName();
-				
-				if (accessRequest.equals(AccessType.WRITE))
-					break; //if write, break after first walk through
-				
-			}
-			return ret;
-		}
-		out.printErrln("We didn't get any envelopes [error in processing].");
-		return super.deserializeForRoot();
-	}
-	 
 
 	/**
 	 * Constructor needed for the command factory in commander
@@ -277,49 +216,49 @@ public class JAliEnCommandaccess extends JAliEnBaseCommand {
 			}
 
 			if (!accessRequest.equals(AccessType.NULL) && (arg.hasNext()) ) {
-					lfnName = arg.next();
+				lfnName = arg.next();
 
-					if(accessRequest.equals(AccessType.WRITE))
-						if (arg.hasNext())
-							localFileName = arg.next();
-						else
-							out.printErrln("Missing local file name for size and checksum consideration [error in request].");
-					
-					
-					if (arg.hasNext()) {
-							final StringTokenizer st = new StringTokenizer(arg.next(), ",");
-							while (st.hasMoreElements()) {
-								String spec = st.nextToken();
-								if (spec.contains("::")) {
-									if (spec.indexOf("::") != spec.lastIndexOf("::")) { // any SE spec
-										if (spec.startsWith("!")) // an exSE spec
-											exses.add(spec.toUpperCase());
-										else {// an SE spec
-											ses.add(spec.toUpperCase());
-											referenceCount++;
-										}
-									}
-								} else if (spec.contains(":")) {// a qosTag:count spec
-									try {
+				if(accessRequest.equals(AccessType.WRITE))
+					if (arg.hasNext())
+						localFileName = arg.next();
+					else
+						out.printErrln("Missing local file name for size and checksum consideration [error in request].");
 
-										int c = Integer.parseInt(spec.substring(spec.indexOf(':') + 1));
-										if (c > 0) {
-											qos.put(spec.substring(0, spec.indexOf(':')), Integer.valueOf(c));
-											referenceCount = referenceCount + c;
-										} else
-											throw new JAliEnCommandException();
 
-									} catch (Exception e) {
-										throw new JAliEnCommandException();
-									}
-								} else if (!spec.equals(""))
-									throw new JAliEnCommandException();
+				if (arg.hasNext()) {
+					final StringTokenizer st = new StringTokenizer(arg.next(), ",");
+					while (st.hasMoreElements()) {
+						String spec = st.nextToken();
+						if (spec.contains("::")) {
+							if (spec.indexOf("::") != spec.lastIndexOf("::")) { // any SE spec
+								if (spec.startsWith("!")) // an exSE spec
+									exses.add(spec.toUpperCase());
+								else {// an SE spec
+									ses.add(spec.toUpperCase());
+									referenceCount++;
+								}
 							}
-						
-						
+						} else if (spec.contains(":")) {// a qosTag:count spec
+							try {
+
+								int c = Integer.parseInt(spec.substring(spec.indexOf(':') + 1));
+								if (c > 0) {
+									qos.put(spec.substring(0, spec.indexOf(':')), Integer.valueOf(c));
+									referenceCount = referenceCount + c;
+								} else
+									throw new JAliEnCommandException();
+
+							} catch (Exception e) {
+								throw new JAliEnCommandException();
+							}
+						} else if (!spec.equals(""))
+							throw new JAliEnCommandException();
 					}
-					
-					
+
+
+				}
+
+
 			} else
 				out.printErrln("Illegal Request type specified [error in request].");
 
