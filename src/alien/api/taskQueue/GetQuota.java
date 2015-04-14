967177d97d55069bc93112afe3c6bdeb5d8f4269
psvirin <psvirin@cern.ch>
2015-04-14 18:52:17 +0200

    JCentral: runs quota routines

---
+++ src/alien/api/taskQueue/GetQuota.java
@@ -0,0 +1,30 @@
+package alien.api.taskQueue;
+
+import alien.api.Request;
+import alien.quotas.Quota;
+import alien.quotas.QuotaUtilities;
+import alien.taskQueue.TaskQueueUtils;
+import alien.user.AliEnPrincipal;
+
+public class GetQuota extends Request {
+	private String username;
+	private Quota q;
+	
+	/**
+	 * @param user 
+	 * @param role 
+	 * @param queueId
+	 */
+	public GetQuota(final AliEnPrincipal user) {
+		this.username = user.getName();
+	}
+	
+	@Override
+	public void run() {
+		this.q = QuotaUtilities.getJobQuota( this.username );		
+	}
+	
+	public Quota getQuota(){
+		return this.q;
+	}
+}
