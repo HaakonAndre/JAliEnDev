22a8691b01d542bb1f2ec289702c34ff7929486d
psvirin <psvirin@cern.ch>
2015-04-14 19:11:10 +0200

    JBox: added fquota command

--- src/alien/shell/commands/JAliEnCommandfquota.java
+++ src/alien/shell/commands/JAliEnCommandfquota.java
@@ -2,28 +2,41 @@ package alien.shell.commands;
 
 import java.util.ArrayList;
 
+import alien.quotas.FileQuota;
 import joptsimple.OptionException;
 
 public class JAliEnCommandfquota extends JAliEnBaseCommand {
 
-	@Override
-	public void run() {
-		// TODO Auto-generated method stub
-
+	public void run() { 
+		String username = commander.user.getName();
+		
+		//Quota q = QuotaUtilities.getJobQuota( username );
+		FileQuota q = commander.q_api.getFileQuota();
+		if( q == null ){
+			out.printErrln("No jobs quota found for user " + username );
+			return;
+		}
+/*		out.setField( "user", new String( q.user ) );
+		out.setField( "priority", Float.toString( q.priority ) );
+		out.setField( "maxParallelJobs", Float.toString( q.maxparallelJobs ) );
+		out.setField( "computed priority", Float.toString( q.computedpriority ) );
+		out.setField( "maxUnfinishedJobs", Float.toString( q.maxUnfinishedJobs ) );
+		out.setField( "maxTotalRunningTime", Float.toString( q.maxTotalRunningTime ) );
+		*/			
 	}
 
 	@Override
 	public void printHelp() {
-		// TODO Auto-generated method stub
-
+		out.printOutln();
+		out.printOutln(helpUsage("fquota",""));		
+		out.printOutln();
 	}
 
 	@Override
 	public boolean canRunWithoutArguments() {
-		// TODO Auto-generated method stub
-		return false;
+		return true;
 	}
-
+	
 	public JAliEnCommandfquota(JAliEnCOMMander commander, UIPrintWriter out, final ArrayList<String> alArguments) throws OptionException {
 		super(commander, out, alArguments);
 	}
