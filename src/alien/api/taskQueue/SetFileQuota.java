7c76daac99ddeeeb21bff09f1a5d8890fed49f08
psvirin <psvirin@cern.ch>
2015-04-16 17:41:43 +0200

    JCentral: added set file quota

---
+++ src/alien/api/taskQueue/SetFileQuota.java
@@ -0,0 +1,41 @@
+package alien.api.taskQueue;
+
+import alien.api.Request;
+import alien.quotas.FileQuota;
+import alien.quotas.QuotaUtilities;
+import alien.taskQueue.TaskQueueUtils;
+import alien.user.AliEnPrincipal;
+
+public class SetFileQuota extends Request {
+	private boolean succeeded;
+	private boolean isAdmin;
+	private String field;
+	private String value;
+	private String username;
+	
+	/**
+	 * @param user 
+	 * @param role 
+	 * @param queueId
+	 */
+	public SetFileQuota(final AliEnPrincipal user, String fld, String val) {
+		this.isAdmin = user.canBecome("admin");
+		this.field = fld;
+		this.value = val;
+		this.username = user.getName();
+	}
+	
+	@Override
+	public void run() {
+		if( !this.isAdmin )
+			return;
+		
+		this.succeeded = QuotaUtilities.saveFileQuota( this.username, 
+														this.field,
+														this.value);		
+	}
+	
+	public boolean getSucceeded(){
+		return this.succeeded;
+	}
+}
