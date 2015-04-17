68ad2bace262cd9dec7258de324e97bda1aa9afa
psvirin <psvirin@cern.ch>
2015-04-17 15:58:55 +0200

    JBox: save file quota functionality update

--- src/alien/shell/commands/JAliEnCommandfquota.java
+++ src/alien/shell/commands/JAliEnCommandfquota.java
@@ -11,7 +11,7 @@ public class JAliEnCommandfquota extends JAliEnBaseCommand {
 	private String command;
 	private String user_to_set;
 	private String param_to_set;
-	private Integer value_to_set;
+	private Long value_to_set;
 	
 	private final ArrayList<String> allowed_fields = new ArrayList<String>( 
 			Arrays.asList( "maxNbFiles",
@@ -38,17 +38,19 @@ public class JAliEnCommandfquota extends JAliEnBaseCommand {
 		}
 		
 		if( command.equals("set") ){
-			if( this.param_to_set==null || 
-					this.value_to_set==null || 
+			if( this.param_to_set==null ){
+				out.printErrln("Error in parameter name" );
+				return;
+			}
+			else if( this.value_to_set==null || 
 					this.value_to_set==0 ){
-				out.printErrln("Error in parameter or value" );
+				out.printErrln("Error in value" );
 				printHelp();
 				return;
 			}
 			// run the update
 			commander.q_api.setFileQuota( this.param_to_set, this.value_to_set.toString() );
 		}
-				
 	}
 
 	@Override
@@ -75,8 +77,10 @@ public class JAliEnCommandfquota extends JAliEnBaseCommand {
 	public JAliEnCommandfquota(JAliEnCOMMander commander, UIPrintWriter out, final ArrayList<String> alArguments) throws OptionException {
 		super(commander, out, alArguments);
 		this.isAdmin = commander.getUser().canBecome("admin");
-		if( alArguments.size() > 0 )
-			this.command = alArguments.get(0);
+		if( alArguments.size() == 0 )
+			return;
+		this.command = alArguments.get(0);
+		System.out.println( alArguments );
 		if( this.command.equals("set") && alArguments.size()==4 ){
 			this.user_to_set = alArguments.get(1);
 			String param = alArguments.get(2);
@@ -84,7 +88,7 @@ public class JAliEnCommandfquota extends JAliEnBaseCommand {
 				return;
 			this.param_to_set = param;
 			try{
-				this.value_to_set = Integer.parseInt( alArguments.get(3) );
+				this.value_to_set = Long.parseLong( alArguments.get(3) );
 			}
 			catch( Exception e ){}
 		}
