58f769340116ab8d4ef9b1947afab36fbfb253c9
psvirin <psvirin@cern.ch>
2015-05-08 12:21:22 +0200

    JBox: basic listSEDistance functionality

--- src/alien/shell/commands/JAliEnCommandlistSEDistance.java
+++ src/alien/shell/commands/JAliEnCommandlistSEDistance.java
@@ -1,11 +1,17 @@
 package alien.shell.commands;
 
 import java.util.ArrayList;
+import java.util.List;
 
 import joptsimple.OptionException;
+import joptsimple.OptionParser;
+import joptsimple.OptionSet;
 
 public class JAliEnCommandlistSEDistance extends JAliEnBaseCommand {
-
+	private boolean useWriteMetrics;
+	private String site;
+	private String lfn_name;
+	
 	@Override
 	public void run() {
 		// TODO Auto-generated method stub
@@ -34,5 +40,26 @@ public class JAliEnCommandlistSEDistance extends JAliEnBaseCommand {
 	public JAliEnCommandlistSEDistance(JAliEnCOMMander commander, UIPrintWriter out,
 			final ArrayList<String> alArguments) throws OptionException {
 		super(commander, out, alArguments);
+		
+		this.useWriteMetrics = true;
+		int argLen = alArguments.size();
+		if( argLen==0 )
+			return;
+		String arg = alArguments.get(0);
+		if( !arg.equals("read") && !arg.equals("write") )
+			this.site = arg;
+		else
+			this.useWriteMetrics = (arg.equals("write"));
+		if( argLen==1 )
+			return;
+		arg = alArguments.get(1);
+		if( !arg.equals("read") && !arg.equals("write") )
+			this.site = this.lfn_name;
+		else
+			this.useWriteMetrics = (arg.equals("write"));
+		if( argLen==2 )
+			return;
+		if( !this.useWriteMetrics && this.lfn_name==null && argLen==3 )
+			this.lfn_name = arg;
 	}
 }
