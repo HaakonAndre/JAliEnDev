0322b14633257a10bbd87af37845cb5e309aab82
psvirin <psvirin@cern.ch>
2015-05-13 18:02:33 +0200

    JCentral: update for list SE distance

--- src/alien/api/catalogue/ListSEDistance.java
+++ src/alien/api/catalogue/ListSEDistance.java
@@ -21,7 +21,7 @@ import java.util.List;
  */
 public class ListSEDistance extends Request {	
 	private static final long serialVersionUID = 726995834931008148L;
-	private LFN lfn;
+	private String lfn_name;
 	private boolean write;
 	private String site;
 	private List<SE> ses;
@@ -39,8 +39,7 @@ public class ListSEDistance extends Request {
 							final String lfn_name){
 		setRequestUser(user);
 		setRoleRequest(role);
-		if( lfn_name!=null && lfn_name.length()!=0 )
-			this.lfn = LFNUtils.getLFN(lfn_name);
+		this.lfn_name = lfn_name;		
 		this.write = write;		
 		if(sitename==null || sitename.length()==0)
 			this.site = ConfigUtils.getConfig().gets("alice_close_site", "CERN").trim();
@@ -52,15 +51,18 @@ public class ListSEDistance extends Request {
 	public void run() {
 		// this is for write
 		if( this.write ){
-			this.ses = SEUtils.getClosestSEs(this.site, this.write);
+			this.ses = SEUtils.getClosestSEs(this.site, true);
 			return;
 		}
 				
 		//for read with lfn specified
 		this.ses = new ArrayList<SE>();
-		if( this.lfn==null )
+		if( this.lfn_name==null )
 			return;
-		List<PFN> lp = SEUtils.sortBySite( this.lfn.whereis(), this.site, true, false);
+		LFN lfn = null;
+		if( this.lfn_name!=null && this.lfn_name.length()!=0 )
+			lfn = LFNUtils.getLFN(this.lfn_name);
+		List<PFN> lp = SEUtils.sortBySite(lfn.whereis(), this.site, true, false);
 		
 		if( lp==null )
 			return;
