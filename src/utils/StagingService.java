b90b4f2919c888fe8951b11feaeb4e5efbea03d2
Costin Grigoras <costin.grigoras@cern.ch>
2016-06-28 16:00:56 +0200

    StagingService: split staging in a synchronous and an asynchronous part

--- src/utils/StagingService.java
+++ src/utils/StagingService.java
@@ -1,6 +1,7 @@
 package utils;
 
 import java.io.IOException;
+import java.util.HashSet;
 import java.util.Set;
 import java.util.concurrent.LinkedBlockingQueue;
 import java.util.concurrent.ThreadPoolExecutor;
@@ -12,6 +13,8 @@ import alien.catalogue.LFNUtils;
 import alien.catalogue.PFN;
 import alien.config.ConfigUtils;
 import alien.io.protocols.Factory;
+import alien.se.SE;
+import alien.se.SEUtils;
 import lazyj.DBFunctions;
 import lazyj.Format;
 
@@ -24,6 +27,7 @@ import lazyj.Format;
 public class StagingService {
 	private static final LinkedBlockingQueue<Runnable> executorQueue = new LinkedBlockingQueue<>();
 	private static final ThreadPoolExecutor executor = new ThreadPoolExecutor(32, 32, 5, TimeUnit.SECONDS, executorQueue);
+	static final ThreadPoolExecutor bgexecutor = new ThreadPoolExecutor(32, 32, 5, TimeUnit.SECONDS, executorQueue);
 
 	static final AtomicLong PREPARED_COMMANDS = new AtomicLong();
 
@@ -33,6 +37,23 @@ public class StagingService {
 
 	static DBFunctions getDB() {
 		return ConfigUtils.getDB("alice_users");
+	}
+
+	private static class StagePFN implements Runnable {
+		final PFN pfn;
+
+		public StagePFN(final PFN pfn) {
+			this.pfn = pfn;
+		}
+
+		@Override
+		public void run() {
+			try {
+				Factory.xrootd.prepare(pfn);
+			} catch (final IOException ioe) {
+				System.err.println("Could not background stage: " + pfn.getPFN() + ", the error message was:\n" + ioe.getMessage());
+			}
+		}
 	}
 
 	private static class StageLFN implements Runnable {
@@ -53,12 +74,15 @@ public class StagingService {
 
 				if (pfns != null)
 					for (final PFN p : pfns)
-						try {
-							Factory.xrootd.prepareCond(p);
-						} catch (final IOException ioe) {
-							System.err.println("Could not stage: " + p.getPFN() + ", the error message was:\n" + ioe.getMessage());
-							delete = false;
-						}
+						if (syncSEs.size() == 0 || syncSEs.contains(Integer.valueOf(p.seNumber)))
+							try {
+								Factory.xrootd.prepareCond(p);
+							} catch (final IOException ioe) {
+								System.err.println("Could not stage: " + p.getPFN() + ", the error message was:\n" + ioe.getMessage());
+								delete = false;
+							}
+						else
+							bgexecutor.submit(new StagePFN(p));
 			}
 
 			try (DBFunctions db = getDB()) {
@@ -72,15 +96,27 @@ public class StagingService {
 		}
 	}
 
+	final static Set<Integer> syncSEs = new HashSet<>();
+
 	/**
 	 * Service entry point
 	 *
 	 * @param args
-	 *            no arguments can be passed
+	 *            SEs (number or full name) for which the staging is done synchronously. For other PFNs the request is queued to be tried, once, at a later time.
 	 * @throws InterruptedException
 	 */
 	public static void main(final String[] args) throws InterruptedException {
 		boolean lastNoWork = false;
+
+		for (final String arg : args)
+			try {
+				syncSEs.add(Integer.valueOf(arg));
+			} catch (@SuppressWarnings("unused") final NumberFormatException nfe) {
+				final SE se = SEUtils.getSE(arg);
+
+				if (se != null)
+					syncSEs.add(Integer.valueOf(se.seNumber));
+			}
 
 		try (DBFunctions db = getDB()) {
 			while (true) {
