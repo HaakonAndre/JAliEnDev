50b357857eb3768c627e9a1160debc3ba85b0a46
Miguel Martinez Pedreira <mmmartin@pcalice92.cern.ch>
2016-08-11 17:34:40 +0200

    Optimizers: father, GuidTable (monitor and create G tables) and LTables (monitors for big LFN tables)

---
+++ src/alien/JOptimizers.java
@@ -0,0 +1,19 @@
+package alien;
+
+import alien.optimizers.Optimizer;
+
+/**
+ * @author mmmartin
+ * @since Aug 9, 2016
+ */
+public class JOptimizers {
+
+	/**
+	 * @param args
+	 * @throws Exception
+	 */
+	public static void main(final String[] args) throws Exception {
+		Optimizer opt = new Optimizer();
+		opt.start();
+	}
+}
---
+++ src/alien/optimizers/catalogue/GuidTable.java
@@ -0,0 +1,174 @@
+package alien.optimizers.catalogue;
+
+import java.util.UUID;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import alien.catalogue.GUIDUtils;
+import alien.config.ConfigUtils;
+import alien.optimizers.Optimizer;
+import lazyj.DBFunctions;
+import lazyj.mail.Mail;
+import lazyj.mail.Sendmail;
+
+/**
+ * @author Miguel
+ * @since Aug 9, 2016
+ */
+public class GuidTable extends Optimizer {
+
+	static transient final Logger logger = ConfigUtils.getLogger(GuidTable.class.getCanonicalName());
+
+	final static int maxCount = 50000000; // 50M
+	final static int warnCount = 40000000; // 40M
+	int count;
+
+	public void run() {
+		this.setSleepPeriod(3600 * 1000); // 1h
+
+		logger.log(Level.INFO, "GuidTable optimizer starts");
+
+		try (DBFunctions db = ConfigUtils.getDB("alice_users")) {
+			if (db == null) {
+				logger.log(Level.INFO, "GuidTable could not get a DB!");
+				return;
+			}
+
+			while (true) {
+				logger.log(Level.INFO, "GuidTable wakes up!: going to get G tables counts with max: " + maxCount);
+
+				// Get count of latest G tables
+				db.setReadOnly(true);
+				db.query("select max(tableName) from GUIDINDEX where guidTime is not null");
+				db.moveNext();
+				int tableNumber = db.geti(1);
+
+				db.setReadOnly(true);
+				db.query("select count(1) from G" + tableNumber + "L_PFN");
+				db.moveNext();
+				count = db.geti(1);
+
+				if (count > maxCount) {
+					// new G table
+					createNewGTable(db, tableNumber + 1);
+				}
+				else {
+					db.setReadOnly(true);
+					db.query("select count(1) from G" + tableNumber + "L");
+					db.moveNext();
+					count = db.geti(1);
+
+					if (count > maxCount) {
+						// new G table
+						createNewGTable(db, tableNumber + 1);
+					}
+				}
+
+				if (count > warnCount) {
+					String admins = ConfigUtils.getConfig().gets("mail_admins"); // comma separated list of emails in config.properties 'mail_admins'
+					if (admins != null && admins.length() > 0) {
+						Mail m = new Mail();
+						m.sSubject = "JAliEn CS: G table filling up";
+						m.sBody = "The table G" + tableNumber + "L has passed " + warnCount + " entries and will be soon renewed!";
+						m.sFrom = "JAliEnMaster@cern.ch";
+						m.sTo = admins;
+						final Sendmail s = new Sendmail(m.sFrom, "cernmx.cern.ch");
+
+						if (!s.send(m))
+							logger.log(Level.SEVERE, "Could not send notification email: " + s.sError);
+					}
+				}
+
+				try {
+					logger.log(Level.INFO, "GuidTable sleeps " + this.getSleepPeriod());
+					sleep(this.getSleepPeriod());
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+	}
+
+	private void createNewGTable(DBFunctions db, int newTable) {
+		logger.log(Level.INFO, "GuidTable: creating new table: " + newTable);
+		String admins = ConfigUtils.getConfig().gets("mail_admins"); // comma separated list of emails in config.properties 'mail_admins'
+		if (admins != null && admins.length() > 0) {
+			Mail m = new Mail();
+			m.sSubject = "JAliEn CS: new G" + newTable + "L tables";
+			m.sBody = "The table G" + newTable + "L has been inserted now.\n\n" + "The previous table had " + count + " entries.";
+			m.sFrom = "JAliEnMaster@cern.ch";
+			m.sTo = admins;
+			final Sendmail s = new Sendmail(m.sFrom, "cernmx.cern.ch");
+
+			if (!s.send(m))
+				logger.log(Level.SEVERE, "Could not send notification email: " + s.sError);
+
+		}
+
+		String sql = "CREATE TABLE `G" + newTable + "L` (" + " " + "`guidId` int(11) NOT NULL AUTO_INCREMENT," + " `ctime` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,"
+				+ " `owner` varchar(20) COLLATE latin1_general_cs DEFAULT NULL," + " `ref` int(11) DEFAULT '0'," + " `jobid` int(11) DEFAULT NULL,"
+				+ " `seStringlist` varchar(255) COLLATE latin1_general_cs NOT NULL DEFAULT ','," + " `seAutoStringlist` varchar(255) COLLATE latin1_general_cs NOT NULL DEFAULT ',',"
+				+ " `aclId` int(11) DEFAULT NULL," + " `expiretime` datetime DEFAULT NULL," + " `size` bigint(20) NOT NULL DEFAULT '0',"
+				+ " `gowner` varchar(20) COLLATE latin1_general_cs DEFAULT NULL," + " `guid` binary(16) DEFAULT NULL," + " `type` char(1) COLLATE latin1_general_cs DEFAULT NULL,"
+				+ " `md5` varchar(32) COLLATE latin1_general_cs DEFAULT NULL," + " `perm` char(3) COLLATE latin1_general_cs DEFAULT NULL," + " PRIMARY KEY (`guidId`)," + " UNIQUE KEY `guid` (`guid`),"
+				+ " KEY `seStringlist` (`seStringlist`)," + " KEY `ctime` (`ctime`)" + ") ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_general_cs;";
+
+		db.setReadOnly(false);
+		if (!db.query(sql)) {
+			logger.log(Level.SEVERE, "Could not create table: G" + newTable + "L: " + sql);
+			return;
+		}
+		logger.log(Level.INFO, sql);
+
+		sql = "CREATE TABLE `G" + newTable + "L_PFN` (" + "  `guidId` int(11) NOT NULL," + "  `pfn` varchar(255) COLLATE latin1_general_cs DEFAULT NULL," + "  `seNumber` int(11) NOT NULL,"
+				+ "  KEY `guid_ind` (`guidId`)," + "  KEY `seNumber` (`seNumber`)," + "  CONSTRAINT `G" + newTable
+				+ "L_PFN_ibfk_1` FOREIGN KEY (`seNumber`) REFERENCES `SE` (`seNumber`) ON DELETE CASCADE," + "  CONSTRAINT `G" + newTable + "L_PFN_ibfk_2` FOREIGN KEY (`guidId`) REFERENCES `G"
+				+ newTable + "L` (`guidId`) ON DELETE CASCADE) " + " ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_general_cs;";
+
+		db.setReadOnly(false);
+		if (!db.query(sql)) {
+			logger.log(Level.SEVERE, "Could not create table: G" + newTable + "L_PFN: " + sql);
+			db.query("DROP TABLE IF EXISTS G" + newTable + "L");
+			return;
+		}
+		logger.log(Level.INFO, sql);
+
+		sql = "CREATE TABLE `G" + newTable + "L_QUOTA` (" + "  `user` varchar(64) COLLATE latin1_general_cs NOT NULL," + "  `nbFiles` int(11) NOT NULL," + "  `totalSize` bigint(20) NOT NULL,"
+				+ "  KEY `user_ind` (`user`)" + ") ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_general_cs;";
+
+		db.setReadOnly(false);
+		if (!db.query(sql)) {
+			logger.log(Level.SEVERE, "Could not create table: G" + newTable + "L_QUOTA: " + sql);
+			db.query("DROP TABLE IF EXISTS G" + newTable + "L_PFN, G" + newTable + "L");
+			return;
+		}
+		logger.log(Level.INFO, sql);
+
+		sql = "CREATE TABLE `G" + newTable + "L_REF` (" + "  `guidId` int(11) NOT NULL," + "  `lfnRef` varchar(20) COLLATE latin1_general_cs NOT NULL," + "  KEY `guidId` (`guidId`),"
+				+ "  KEY `lfnRef` (`lfnRef`)," + "  CONSTRAINT `G" + newTable + "L_REF_ibfk_1` FOREIGN KEY (`guidId`) REFERENCES `G" + newTable + "L` (`guidId`) ON DELETE CASCADE"
+				+ ") ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_general_cs;";
+
+		db.setReadOnly(false);
+		if (!db.query(sql)) {
+			logger.log(Level.SEVERE, "Could not create table: G" + newTable + "L_REF: " + sql);
+			db.query("DROP TABLE IF EXISTS G" + newTable + "L_PFN, G" + newTable + "L_QUOTA, G" + newTable + "L");
+			return;
+		}
+		logger.log(Level.INFO, sql);
+
+		// Inserting in 10min from now in the new tables
+		UUID uuid = GUIDUtils.generateTimeUUID(System.currentTimeMillis() + 10 * 60 * 1000);
+		String toInsert = GUIDUtils.getIndexTime(uuid).concat("00000000");
+
+		sql = "INSERT INTO GUIDINDEX values (0, 8, " + newTable + ", '" + toInsert + "', NULL)";
+		db.setReadOnly(false);
+		if (!db.query(sql)) {
+			logger.log(Level.SEVERE, "Could not insert into GUIDINDEX: " + sql);
+			db.query("DROP TABLE IF EXISTS G" + newTable + "L_REF, G" + newTable + "L_PFN, G" + newTable + "L_QUOTA, G" + newTable + "L");
+			return;
+		}
+		logger.log(Level.INFO, sql);
+
+	}
+
+}
