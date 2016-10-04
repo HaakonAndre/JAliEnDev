d340c6c69faf55fd01fafdbca9e3b5ac034bea16
Pavlo Svirin <pavlo.svirin@cern.ch>
2016-10-04 12:15:15 -0400

    Dropped TitanJobStatus class

--- src/alien/site/TitanJobService.java
+++ src/alien/site/TitanJobService.java
@@ -1396,28 +1396,6 @@ public class TitanJobService extends Thread implements MonitoringObject {
 		}
 	}
 
-	/*class TitanJobStatus{
-		public final int rank;
-		public Long queueId;
-		public String  jobFolder;
-		public String status;
-		public int executionCode;
-		public int validationCode;
-		final TitanBatchInfo batch;
-		public TitanJobStatus(int r, Long qid, String job_folder, 
-						String st, int exec_code, 
-						int val_code, TitanBatchInfo bi){
-			rank = r;
-			queueId = qid;
-			jobFolder = job_folder;
-			status = st;
-			executionCode = exec_code;
-			validationCode = val_code;
-			batch = bi;
-		}
-
-	}; */
-
 	class TitanBatchInfo{
 		public final Long pbsJobId;
 		public final String dbName;
