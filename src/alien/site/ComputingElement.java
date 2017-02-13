fdf906fca14f50b2fc28d83744db3700d7794f35
Miguel Martinez Pedreira <mmmartin@mmmartin-macbook.dyndns.cern.ch>
2017-02-13 16:56:57 +0100

    Add sitemap to CE

--- src/alien/site/ComputingElement.java
+++ src/alien/site/ComputingElement.java
@@ -1,12 +1,16 @@
 package alien.site;
 
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
 import alien.api.DispatchSSLServer;
 import alien.user.JAKeyStore;
 
-
 public class ComputingElement extends Thread {
+
+	private final Map<String, String> env = System.getenv();
+	private HashMap<String, Object> siteMap = null;
 
 	public ComputingElement() {
 		try {
@@ -29,10 +33,13 @@ public class ComputingElement extends Thread {
 			e1.printStackTrace();
 		}
 
+		siteMap = (new SiteMap()).getSiteParameters();
+
 		// while (true) {
 		// here we would have to poll the queue info and submit jobAgents....
 
 		// }
 
 	}
+	
 }
