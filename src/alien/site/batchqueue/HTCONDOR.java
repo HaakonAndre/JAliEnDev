50eeca07687e0c25247e40200f9047cccdfe0d32
Ivan <ivan.shelest@cern.ch>
2017-09-07 18:49:59 +0200

    constructor and submit wip

--- src/alien/site/batchqueue/HTCONDOR.java
+++ src/alien/site/batchqueue/HTCONDOR.java
@@ -2,8 +2,12 @@ package alien.site.batchqueue;
 
 import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.nio.charset.Charset;
 import java.sql.Timestamp;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
@@ -12,6 +16,7 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.concurrent.TimeUnit;
 import java.util.logging.Logger;
+import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 import java.util.Date;
 
@@ -24,8 +29,13 @@ import alien.log.LogUtils;
  */
 public class HTCONDOR extends BatchQueue {
 
-	private Map<String, String> environment = System.getenv();
-	private ArrayList<Process> process_list = new ArrayList<Process>();
+	private Map<String, String> _environment;
+	private ArrayList<Process> _process_list;
+	private int _counter;
+	private String _log_dir_path;
+	private String _submit_cmd;
+	private String _submit_args;
+	private String _user_name;
 
 	/**
 	 * @param conf
@@ -38,6 +48,27 @@ public class HTCONDOR extends BatchQueue {
 		logger = LogUtils.redirectToCustomHandler(logger, ((String) config.get("host_logdir")) + "JAliEn." + (new Timestamp(System.currentTimeMillis()).getTime() + ".out"));
 
 		logger.info("This VO-Box is " + config.get("ALIEN_CM_AS_LDAP_PROXY") + ", site is " + config.get("site_accountName"));
+		
+		this._environment = System.getenv();
+		this._process_list = new ArrayList<Process>();
+		this._counter = 0;
+		this._log_dir_path = (String) config.get("LOG_DIR");
+		this._submit_cmd = (config.get("CE_SUBMITCMD") != null ? (String)config.get("CE_SUBMITCMD") : "condor_submit");
+		this._submit_args = (_environment.get("SUBMIT_ARGS") != null ? _environment.get("SUBMIT_ARGS") : "");
+		if(_environment.get("LOGNAME") != null) {
+			this._user_name = _environment.get("LOGNAME");
+		}
+		else if(_environment.get("USER") != null) {
+			this._user_name = _environment.get("LOGNAME");
+		}
+		else {
+			this._user_name = "Unknown User"; // TODO: get process name from PID?
+		}
+		// TODO: WIP
+//		$self->{KILL_CMD} = ( $self->{CONFIG}->{CE_KILLCMD} or "condor_rm" );
+//		  $self->{STATUS_CMD} = ( $self->{CONFIG}->{CE_STATUSCMD} or "condor_q" );
+//
+//		  $self->{GET_QUEUE_STATUS} = $self->{STATUS_CMD};
 	}
 
 	@Override
@@ -48,7 +79,7 @@ public class HTCONDOR extends BatchQueue {
 		DateFormat date_format = new SimpleDateFormat("yyyy-MM-dd");
 		String current_date_str = date_format.format(new Date());
 		
-		String log_folder_path = String.format("%s/%s", environment.get("HTCONDOR_LOG_PATH"), current_date_str);
+		String log_folder_path = String.format("%s/%s", _environment.get("HTCONDOR_LOG_PATH"), current_date_str);
 		File log_folder = new File(log_folder_path);
 		if (!(log_folder.exists()) || !(log_folder.isDirectory())) {
 			try {
@@ -62,7 +93,7 @@ public class HTCONDOR extends BatchQueue {
 		String log_cmd = String.format("log = %s.log", file_base_name);
 		String out_cmd = "";
 		String err_cmd = "";
-		File enable_sandbox_file= new File(environment.get("HOME") + "/enable-sandbox"); 
+		File enable_sandbox_file= new File(_environment.get("HOME") + "/enable-sandbox"); 
 		if (enable_sandbox_file.exists() || (this.logger.getLevel() != null)) {
 			out_cmd = String.format("output = %s.out", file_base_name);
 			err_cmd = String.format("error = %s.err", file_base_name);
@@ -73,10 +104,120 @@ public class HTCONDOR extends BatchQueue {
 				+ "JobStatus <= 2 && ( CurrentTime - EnteredCurrentStatus > 172800 )";
 		String per_remove = "periodic_remove = CurrentTime - QDate > 259200";
 		String osb = "+TransferOutput = \"\"";
+		
+		// ===========
+		
 		String submit_cmd = String.format("cmd = %s\n", script);
-		if (environment.get("HTCONDOR_LOG_PATH") != null) {
+		if (_environment.get("HTCONDOR_LOG_PATH") != null) {
 			submit_cmd += String.format("%s\n%s\n%s\n", out_cmd, err_cmd, log_cmd);
 		}
+		
+		// --- via JobRouter or direct
+		
+		if (_environment.get("USE_JOB_ROUTER") != null) {
+			submit_cmd += ""
+					+ "universe = vanilla\n"
+					+ "+WantJobRouter = True\n"
+					+ "job_lease_duration = 7200\n"
+					+ "ShouldTransferFiles = YES\n";
+		}
+		else if(_environment.get("GRID_RESOURCE") != null) {
+			submit_cmd += ""
+					+ "universe = grid\n"
+					+ String.format("grid_resource = %s\n", _environment.get("GRID_RESOURCE"));
+		}
+		
+		// --- further common attributes
+		
+		if(_environment.get("GRID_RESOURCE") != null) {
+			submit_cmd += "+WantExternalCloud = True\n";
+		}
+		submit_cmd += ""
+				+ "$osb\n"
+				+ "$per_hold\n"
+				+ "$per_remove\n"
+				+ "use_x509userproxy = true\n";
+
+		String env_cmd = String.format("ALIEN_CM_AS_LDAP_PROXY=\'%s\' ", cm)
+				+ String.format("ALIEN_JOBAGENT_ID=\'%s\'", _environment.get("ALIEN_JOBAGENT_ID"));
+		submit_cmd += String.format("environment = \"%s\"\n", env_cmd);
+
+		// --- allow preceding attributes to be overridden and others added if needed
+		
+		String custom_jdl_path = String.format("%s/custom-classad.jdl", _environment.get("HOME"));
+		String custom_attr_str = "\n#\n# custom attributes start\n#\n\n";
+		custom_attr_str += this.readJdlFile(custom_jdl_path);
+		custom_attr_str += "\n#\n# custom attributes end\n#\n\n";
+		submit_cmd += custom_attr_str;
+		logger.info(String.format("Custom attributes added from file: %s.", custom_jdl_path));
+		
+		// --- finally
+
+		submit_cmd += "queue 1\n";
+
+		// =============
+		
+		this._counter++;
+		long time = System.currentTimeMillis() / 1000L;
+		time = time >>> 6;
+		
+		// TODO: WIP
+//		my $jdlFile = AliEn::TMPFile->new({ filename => "htc-submit.$t.jdl" })
+//			    or return $error;
+//
+//			  open(F, '>', $jdlFile) or return $error;
+//			  print F $submit;
+//			  close F or return $error;
+//
+//			  my @lines = $self->_system("$self->{SUBMIT_CMD} $self->{SUBMIT_ARGS} $jdlFile");
+//
+//			  unless ($?) {
+//			    foreach (@lines) {
+//			      chomp;
+//			      $self->info($_);
+//			    }
+//			  }
+	}
+	
+	private String readJdlFile(String path) {
+		String file_contents = "";
+		
+		String line;
+		try {
+		    InputStream fis = new FileInputStream("the_file_name");
+		    InputStreamReader isr = new InputStreamReader(fis);
+		    BufferedReader br = new BufferedReader(isr);
+		    
+			Pattern comment_pattern = Pattern.compile("^\\s*(#.*|//.*)?$");
+			Pattern err_spaces_pattern = Pattern.compile("\\\\\\s*$");
+		    while ((line = br.readLine()) != null) {
+				Matcher comment_matcher = comment_pattern.matcher(line);
+		    	// skip over comment lines
+		    	if(comment_matcher.matches()) {
+		    		continue;
+		    	}
+		    	// remove erroneous spaces
+		    	line.replaceAll(err_spaces_pattern.pattern(), "\\\\\\n");
+		    	if(line.lastIndexOf('\n') == -1) {
+		    		line += '\n';
+		    	}
+		    	file_contents += line;
+		    }
+		    
+		    br.close();
+		    isr.close();
+		    fis.close();
+		} catch (FileNotFoundException e) {
+			logger.info(String.format("Could not find file: %s.", path));
+			e.printStackTrace();
+			return "";
+		} catch (IOException e) {
+			logger.info(String.format("Error while working with file: %s.", path));
+			e.printStackTrace();
+			return file_contents;
+		}
+		
+		return file_contents;
 	}
 
 	@Override
@@ -127,11 +268,11 @@ public class HTCONDOR extends BatchQueue {
 		ArrayList<String> proc_output = new ArrayList<String>();
 		try {
 			Process proc = proc_builder.start();
-			process_list.add(proc);
+			_process_list.add(proc);
 			if(!proc.waitFor(60, TimeUnit.SECONDS)){
 				logger.info(String.format("LCG Timeout for: %s\nKilling the process with id %i", cmd, proc));
 				proc.destroyForcibly();
-				process_list.remove(proc);
+				_process_list.remove(proc);
 				throw new InterruptedException("Timeout");
 			}
 			BufferedReader reader = new BufferedReader(new InputStreamReader(proc.getInputStream()));
