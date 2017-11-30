1b7d64bdbf5934361c12b11fa99144bde64cc215
yuw <volodymyr.yurchenko@cern.ch>
2017-11-30 16:04:14 +0100

    GetMatchJob: set UserCertificate of effective requester

--- src/alien/api/taskQueue/GetMatchJob.java
+++ src/alien/api/taskQueue/GetMatchJob.java
@@ -28,11 +28,11 @@ public class GetMatchJob extends Request {
 	public GetMatchJob(final AliEnPrincipal user, final HashMap<String, Object> siteMap) {
 		setRequestUser(user);
 		this.matchRequest = siteMap;
-		this.matchRequest.put("UserCertificate", this.getRequesterIdentity().getUserCert()[0]);
 	}
 
 	@Override
 	public void run() {
+		this.matchRequest.put("UserCertificate", this.getEffectiveRequester().getUserCert());
 		this.match = JobBroker.getMatchJob(matchRequest);
 	}
 
