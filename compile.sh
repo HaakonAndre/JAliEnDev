daff627bb9cb24c7b547028b147ba7584f942ade
Costin Grigoras <costin.grigoras@cern.ch>
2018-03-19 16:35:00 +0100

    Create two new jar files, for users and CS, all dependencies included

--- compile.sh
+++ compile.sh
@@ -2,7 +2,9 @@
 
 cd `dirname $0`
 
-mkdir build_eclipse &>/dev/null
+TARGETDIR=temp_build_dir
+
+mkdir $TARGETDIR &>/dev/null
 
 CLASSPATH=.:../
 
@@ -14,9 +16,9 @@ export CLASSPATH
 
 cd src
 
-find . -name \*.java | xargs javac -O -g -d ../build_eclipse || exit 1
+find . -name \*.java | xargs javac -O -g -d ../$TARGETDIR || exit 1
 
-cd ../build_eclipse
+cd ../$TARGETDIR
 
 cp ../trusted_authorities.jks .
 
@@ -26,3 +28,28 @@ cp ../config/config.properties ../config/monitoring.properties config/
 
 jar cf ../alien.jar *
 
+## Packaging as a single .jar file for users
+
+for dependency in ../lib/{FarmMonitor.jar,apmon.jar,bcp*.jar,catalina.jar,javax.json-api-*.jar,jline-*.jar,jopt-simple-*.jar,json-simple-*.jar,lazyj.jar,servlet-api.jar,tomcat-*.jar,ca-api*.jar,java-ca-lib*.jar,annotations-api.jar}; do
+    jar -xf $dependency
+done
+
+rm -rf META-INF
+
+jar cf ../alien-users.jar *
+
+## Now all the dependencies in a single file, for central services (+DB drivers, CA, everything else)
+
+for dependency in ../lib/*.jar; do
+    jar -xf $dependency
+done
+
+rm -rf META-INF
+
+jar cf ../alien-cs.jar *
+
+## Cleanup
+
+cd ..
+
+rm -rf $TARGETDIR
