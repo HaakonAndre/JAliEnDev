c025d94465d740cbf00bf0e2e29348f1f0f52993
Costin Grigoras <costin.grigoras@cern.ch>
2018-08-15 11:45:13 +0200

    Automatic handling of GUIDs and more corner cases treated gracefully

--- src/alien/shell/commands/JAliEnCommandwhereis.java
+++ src/alien/shell/commands/JAliEnCommandwhereis.java
@@ -4,6 +4,7 @@ import java.util.List;
 import java.util.Set;
 
 import alien.catalogue.FileSystemUtils;
+import alien.catalogue.GUIDUtils;
 import alien.catalogue.LFN;
 import alien.catalogue.PFN;
 import joptsimple.OptionException;
@@ -39,32 +40,79 @@ public class JAliEnCommandwhereis extends JAliEnBaseCommand {
 
 		String guid = null;
 
-		if (bG)
-			guid = lfnOrGuid;
-		else {
-			final LFN lfn = commander.c_api.getLFN(FileSystemUtils.getAbsolutePath(commander.user.getName(), commander.getCurrentDirName(), lfnOrGuid));
-			if (lfn != null && lfn.guid != null)
-				guid = lfn.guid.toString();
-		}
-		// what message in case of error?
-		if (guid != null) {
-
-			Set<PFN> pfns = commander.c_api.getPFNs(guid);
-
-			if (bR)
-				if (pfns.toArray()[0] != null)
-					if (((PFN) pfns.toArray()[0]).pfn.toLowerCase().startsWith("guid://"))
-						pfns = commander.c_api.getGUID(((PFN) pfns.toArray()[0]).pfn.substring(8, 44)).getPFNs();
-
-			commander.printOutln("the file " + lfnOrGuid.substring(lfnOrGuid.lastIndexOf("/") + 1, lfnOrGuid.length()) + " is in\n");
-			for (final PFN pfn : pfns) {
-
-				final String se = commander.c_api.getSE(pfn.seNumber).seName;
-				commander.printOutln("\t\t SE => " + padRight(se, 30) + " pfn =>" + pfn.pfn + "\n");
+		if (bG) {
+			if (GUIDUtils.isValidGUID(lfnOrGuid))
+				guid = lfnOrGuid;
+			else {
+				commander.printErrln("This is not a valid GUID: " + lfnOrGuid);
+				return;
 			}
 		}
+		else {
+			final LFN lfn = commander.c_api.getLFN(FileSystemUtils.getAbsolutePath(commander.user.getName(), commander.getCurrentDirName(), lfnOrGuid));
+
+			if (lfn != null && lfn.isFile() && lfn.guid != null)
+				guid = lfn.guid.toString();
+			else
+				if (GUIDUtils.isValidGUID(lfnOrGuid)) {
+					bG = true;
+					guid = lfnOrGuid;
+				}
+				else
+					if (lfn != null && !lfn.isFile()) {
+						commander.printErrln("The path you indicated is not a file: " + lfnOrGuid);
+						return;
+					}
+		}
+
+		if (guid != null) {
+			Set<PFN> pfns = commander.c_api.getPFNs(guid);
+
+			if (pfns != null && pfns.size() > 0) {
+				if (bR)
+					if (pfns.toArray()[0] != null) {
+						String archiveGUID = null;
+
+						for (final PFN p : pfns)
+							if (p.pfn.startsWith("guid://"))
+								archiveGUID = p.pfn.substring(8, 44);
+
+						if (archiveGUID != null) {
+							pfns = commander.c_api.getPFNs(archiveGUID);
+
+							if (pfns == null) {
+								commander.printErrln("Archive with GUID " + archiveGUID + " doesn't exist");
+								return;
+							}
+
+							if (pfns.size() == 0) {
+								commander.printErrln("Archive with GUID " + archiveGUID + " doesn't have any replicas, this file cannot be used");
+								return;
+							}
+						}
+						else
+							bR = false; // disable the archive lookup flag because this file is not member of an archive
+					}
+
+				if (bG)
+					commander.printOutln("the GUID " + guid + " is in" + (bR ? "side this archive" : "") + "\n");
+				else
+					commander.printOutln("the file " + lfnOrGuid.substring(lfnOrGuid.lastIndexOf("/") + 1, lfnOrGuid.length()) + " is in" + (bR ? "side this archive" : "") + "\n");
+
+				for (final PFN pfn : pfns) {
+					final String se = pfn.seNumber > 0 ? "SE => " + commander.c_api.getSE(pfn.seNumber).seName : "ZIP archive member";
+					commander.printOutln("\t " + padRight(se, 30) + " pfn => " + pfn.pfn + "\n");
+				}
+			}
+			else
+				if (pfns == null)
+					commander.printErrln("GUID " + guid + " does not exist in the catalogue");
+				else
+					commander.printErrln("GUID " + guid + " has no replicas, this is a lost file");
+		}
 		else
-			commander.printOutln("No such file: [" + lfnOrGuid + "]");
+			commander.printErrln("This file doesn't exist in the catalogue: " + lfnOrGuid);
+
 	}
 
 	/**
