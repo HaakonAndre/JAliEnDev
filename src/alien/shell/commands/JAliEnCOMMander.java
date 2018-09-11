cfd4d4123820cfe9ebcc6a9e363136c1f3a62d81
Costin Grigoras <costin.grigoras@cern.ch>
2018-09-11 11:43:23 +0200

    Stop using AtomicInteger for representing the status (FB warning)

--- src/alien/shell/commands/JAliEnCOMMander.java
+++ src/alien/shell/commands/JAliEnCOMMander.java
@@ -5,7 +5,6 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
-import java.util.concurrent.atomic.AtomicInteger;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -29,6 +28,37 @@ import lazyj.Format;
  * @since June 4, 2011
  */
 public class JAliEnCOMMander extends Thread {
+
+	/**
+	 * Atomic status update of the command execution
+	 * 
+	 * @author costing
+	 * @since 2018-09-11
+	 */
+	public static class CommanderStatus {
+		private int status = 0;
+
+		/**
+		 * @return current value
+		 */
+		public synchronized int get() {
+			return status;
+		}
+
+		/**
+		 * Set the new status code
+		 * 
+		 * @param newValue
+		 * @return the old value
+		 */
+		public synchronized int set(final int newValue) {
+			final int oldValue = status;
+
+			status = newValue;
+
+			return oldValue;
+		}
+	}
 
 	/**
 	 * Logger
@@ -273,7 +303,7 @@ public class JAliEnCOMMander extends Thread {
 	/**
 	 * Current status : 0 = idle, 1 = busy executing a command
 	 */
-	public AtomicInteger status = new AtomicInteger(0);
+	public CommanderStatus status = new CommanderStatus();
 
 	/**
 	 * Set this variable to finish commander's execution
