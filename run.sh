159d50b19d4957ae0b9daa902ac26a2f4a4da94f
Adrian Sevcenco <adrian.sevcenco@cern.ch>
2018-09-30 16:47:35 +0300

    run.sh rework - aware of JALIEN_HOME, independent of place of call, include all found jars in JALIEN_HOME

--- run.sh
+++ run.sh
@@ -1,32 +1,23 @@
 #!/bin/bash
 
-export PATH="$PATH:$HOME/alien/bin:$HOME/alien/api/bin"
-export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:$HOME/alien/lib:$HOME/alien/api/lib"
-
-if [ -f alien-cs.jar ]; then
-    CLASSPATH="alien-cs.jar"
-elif [ -f alien-users.jar ]; then
-    CLASSPATH="alien-users.jar"
-else
-    CLASSPATH="alien.jar"
-
-    for DEP in lib/*.jar; do
-	CLASSPATH="$CLASSPATH:$DEP"
-    done
+if [[ -z "${JALIEN_HOME}" ]]; then
+  ## find the location of jalien script
+  SOURCE="${BASH_SOURCE[0]}"
+  while [ -h "${SOURCE}" ]; do ## resolve $SOURCE until the file is no longer a symlink
+    JALIEN_HOME="$( cd -P "$(dirname "${SOURCE}" )" && pwd )" ##"
+    SOURCE="$(readlink "${SOURCE}")" ##"
+    [[ "${SOURCE}" != /* ]] && SOURCE="${JALIEN_HOME}/${SOURCE}" ## if $SOURCE was a relative symlink, we need to resolve it relative to the path where the symlink file was located
+  done
+  JALIEN_HOME="$(cd -P "$( dirname "${SOURCE}" )" && pwd)" ##"
+  export JALIEN_HOME
 fi
 
-export CLASSPATH
+JAR_LIST_MAIN=$(find "${JALIEN_HOME}" -maxdepth 1 -name "*.jar" -printf "%p:" | sed 's/.$//')
+JAR_LIST_LIB=$(find "${JALIEN_HOME}/lib/" -name "*.jar" -printf "%p:" | sed 's/.$//')
+export CLASSPATH="${JAR_LIST_MAIN}:${JAR_LIST_LIB}"
 
-java \
-	-server \
-	-Xms4G -Xmx4G \
-	-XX:+UseG1GC \
-	-XX:+DisableExplicitGC \
-	-XX:+UseCompressedOops \
-	-XX:+AggressiveOpts \
-	-XX:+OptimizeStringConcat \
-	-XX:MaxTrivialSize=1K \
-	-XX:CompileThreshold=20000 \
-	-Duserid=$(id -u) \
-	-Dcom.sun.jndi.ldap.connect.pool=false \
-	"$@"
+JALIEN_OPTS_DEFAULT="-server -Xms4G -Xmx4G -XX:+UseG1GC -XX:+DisableExplicitGC -XX:+UseCompressedOops -XX:+AggressiveOpts \
+-XX:+OptimizeStringConcat -XX:MaxTrivialSize=1K -XX:CompileThreshold=20000 -Duserid=$(id -u) -Dcom.sun.jndi.ldap.connect.pool=false -Djava.io.tmpdir=/tmp"
+
+CMD="java ${JALIEN_OPTS_DEFAULT}"
+eval "${CMD}" "$@"
