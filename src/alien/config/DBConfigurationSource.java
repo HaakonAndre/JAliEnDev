1b043cd6d18e02a1e19f871743bfc1bde86bddeb
Nikola Hardi <nikola.hardi@cern.ch>
2018-10-09 10:56:21 +0200

    Formatting DBConfigurationSource

--- src/alien/config/DBConfigurationSource.java
+++ src/alien/config/DBConfigurationSource.java
@@ -8,34 +8,33 @@ import lazyj.DBProperties;
 import lazyj.ExtProperties;
 
 class DBConfigurationSource implements ConfigSource {
-  private Map<String, ExtProperties> oldConfig;
-  private boolean isCentralService;
+	private Map<String, ExtProperties> oldConfig;
+	private boolean isCentralService;
 
-  public DBConfigurationSource(final Map<String, ExtProperties> oldConfig, boolean isCentralService) {
-    this.oldConfig = oldConfig;
-    this.isCentralService = isCentralService;
-  }
+	public DBConfigurationSource(final Map<String, ExtProperties> oldConfig, boolean isCentralService) {
+		this.oldConfig = oldConfig;
+		this.isCentralService = isCentralService;
+	}
 
-  public Map<String, ExtProperties> getConfiguration() {
-    Map<String, ExtProperties> dbConfig = new HashMap<String, ExtProperties>();
-    dbConfig.put("config", getConfigFromDB(oldConfig.get("config")));
-    return dbConfig;
-  }
+	public Map<String, ExtProperties> getConfiguration() {
+		Map<String, ExtProperties> dbConfig = new HashMap<String, ExtProperties>();
+		dbConfig.put("config", getConfigFromDB(oldConfig.get("config")));
+		return dbConfig;
+	}
 
-  private ExtProperties getConfigFromDB(final ExtProperties fileConfig) {
-    ExtProperties tmp = new ExtProperties();
+	private ExtProperties getConfigFromDB(final ExtProperties fileConfig) {
+		ExtProperties tmp = new ExtProperties();
 
 		if (isCentralService && fileConfig.getb("jalien.config.hasDBBackend", true)) {
 			final DBFunctions dbAdmin = new DBFunctions(oldConfig.get("admin"));
 
-
 			if (dbAdmin != null) {
 				final DBProperties dbProp = new DBProperties(dbAdmin);
-        dbProp.makeReadOnly();
-        tmp = dbProp;
+				dbProp.makeReadOnly();
+				tmp = dbProp;
 			}
 		}
 
-    return tmp;
-  }
+		return tmp;
+	}
 }
