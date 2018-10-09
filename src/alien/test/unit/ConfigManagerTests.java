7b0c7f8e0561fe9415e50a64c5ff513e2dbabedf
Nikola Hardi <nikola.hardi@cern.ch>
2018-10-09 10:59:27 +0200

    Formatting ConfigManagerTests

--- src/alien/test/unit/ConfigManagerTests.java
+++ src/alien/test/unit/ConfigManagerTests.java
@@ -13,143 +13,143 @@ import alien.config.ConfigSource;
 import lazyj.ExtProperties;
 
 class ConfigManagerTests {
-  private class MockConfigSource implements ConfigSource {
-    private Map<String, ExtProperties> cfgStorage;
+	private class MockConfigSource implements ConfigSource {
+		private Map<String, ExtProperties> cfgStorage;
 
-    public MockConfigSource() {
-      cfgStorage = new HashMap<String, ExtProperties>();
-    }
+		public MockConfigSource() {
+			cfgStorage = new HashMap<String, ExtProperties>();
+		}
 
-    public void set(String file, String key, String value) {
-      if(!cfgStorage.containsKey(file)) {
-        cfgStorage.put(file, new ExtProperties());
-      }
+		public void set(String file, String key, String value) {
+			if (!cfgStorage.containsKey(file)) {
+				cfgStorage.put(file, new ExtProperties());
+			}
 
-      cfgStorage.get(file).set(key, value);
-    }
+			cfgStorage.get(file).set(key, value);
+		}
 
-    public Map<String, ExtProperties> getConfiguration() {
-      return cfgStorage;
-    }
-  }
+		public Map<String, ExtProperties> getConfiguration() {
+			return cfgStorage;
+		}
+	}
 
-  void assertKey(ExtProperties merged, String key, String expected) {
-    String read = merged.gets(key);
-    Assertions.assertEquals(expected, read);
-  }
+	void assertKey(ExtProperties merged, String key, String expected) {
+		String read = merged.gets(key);
+		Assertions.assertEquals(expected, read);
+	}
 
-  void assertKey(ConfigManager mgr, String config, String key, String expected) {
-    assertKey(mgr.getConfiguration().get(config), key, expected);
-  }
+	void assertKey(ConfigManager mgr, String config, String key, String expected) {
+		assertKey(mgr.getConfiguration().get(config), key, expected);
+	}
 
-  @Test
-  void testSingleSource() {
-    MockConfigSource src = new MockConfigSource();
-    ConfigManager cfgManager = new ConfigManager();
+	@Test
+	void testSingleSource() {
+		MockConfigSource src = new MockConfigSource();
+		ConfigManager cfgManager = new ConfigManager();
 
-    src.set("config", "key", "a");
-    cfgManager.registerPrimary(src);
-    assertKey(cfgManager, "config", "key", "a");
-  }
+		src.set("config", "key", "a");
+		cfgManager.registerPrimary(src);
+		assertKey(cfgManager, "config", "key", "a");
+	}
 
-  @Test
-  void testMergeProperties() {
-    ExtProperties front = new ExtProperties();
-    ExtProperties back = new ExtProperties();
-    ExtProperties merged;
+	@Test
+	void testMergeProperties() {
+		ExtProperties front = new ExtProperties();
+		ExtProperties back = new ExtProperties();
+		ExtProperties merged;
 
-    front.set("key", "a");
-    back.set("key", "b");
+		front.set("key", "a");
+		back.set("key", "b");
 
-    merged = ConfigManager.mergeProperties(front, back);
-    assertKey(merged, "key", "a");
+		merged = ConfigManager.mergeProperties(front, back);
+		assertKey(merged, "key", "a");
 
-    merged = ConfigManager.mergeProperties(back, front);
-    assertKey(merged, "key", "b");
-  }
+		merged = ConfigManager.mergeProperties(back, front);
+		assertKey(merged, "key", "b");
+	}
 
-  @Test
-  void testMergePropertiesUpdate() {
-    ExtProperties front = new ExtProperties();
-    ExtProperties back = new ExtProperties();
-    ExtProperties merged;
+	@Test
+	void testMergePropertiesUpdate() {
+		ExtProperties front = new ExtProperties();
+		ExtProperties back = new ExtProperties();
+		ExtProperties merged;
 
-    front.set("otherkey", "c");
-    back.set("key", "b");
-    merged = ConfigManager.mergeProperties(front, back);
+		front.set("otherkey", "c");
+		back.set("key", "b");
+		merged = ConfigManager.mergeProperties(front, back);
 
-    front.set("key", "a");
-    assertKey(merged, "key", "a");
-  }
+		front.set("key", "a");
+		assertKey(merged, "key", "a");
+	}
 
-  @Test
-  void testReloadingConfigurationsSingleSource() {
-    MockConfigSource src = new MockConfigSource();
-    ConfigManager cfgManager = new ConfigManager();
+	@Test
+	void testReloadingConfigurationsSingleSource() {
+		MockConfigSource src = new MockConfigSource();
+		ConfigManager cfgManager = new ConfigManager();
 
-    src.set("config", "key", "a");
-    cfgManager.registerPrimary(src);
+		src.set("config", "key", "a");
+		cfgManager.registerPrimary(src);
 
-    assertKey(cfgManager, "config", "key", "a");
+		assertKey(cfgManager, "config", "key", "a");
 
-    src.set("config", "key", "b");
-    assertKey(cfgManager, "config", "key", "b");
-  }
+		src.set("config", "key", "b");
+		assertKey(cfgManager, "config", "key", "b");
+	}
 
-  @Test
-  void testReloadConfigurationsMultipleSources() {
-    MockConfigSource srcA = new MockConfigSource();
-    MockConfigSource srcB = new MockConfigSource();
-    ConfigManager cfgManager = new ConfigManager();
+	@Test
+	void testReloadConfigurationsMultipleSources() {
+		MockConfigSource srcA = new MockConfigSource();
+		MockConfigSource srcB = new MockConfigSource();
+		ConfigManager cfgManager = new ConfigManager();
 
-    srcA.set("config", "otherkey", "a");
-    srcB.set("config", "key", "b");
+		srcA.set("config", "otherkey", "a");
+		srcB.set("config", "key", "b");
 
-    cfgManager.registerPrimary(srcA);
-    cfgManager.registerFallback(srcB);
-    assertKey(cfgManager, "config", "key", "b");
+		cfgManager.registerPrimary(srcA);
+		cfgManager.registerFallback(srcB);
+		assertKey(cfgManager, "config", "key", "b");
 
-    srcA.set("config", "key", "a");
-    assertKey(cfgManager, "config", "key", "a");
-  }
+		srcA.set("config", "key", "a");
+		assertKey(cfgManager, "config", "key", "a");
+	}
 
-  @Test
-  void testReloadConfigurationPrimary() {
-    MockConfigSource srcA = new MockConfigSource();
-    MockConfigSource srcB = new MockConfigSource();
-    ConfigManager cfgManager = new ConfigManager();
+	@Test
+	void testReloadConfigurationPrimary() {
+		MockConfigSource srcA = new MockConfigSource();
+		MockConfigSource srcB = new MockConfigSource();
+		ConfigManager cfgManager = new ConfigManager();
 
-    srcA.set("config", "otherkey", "x");
-    srcB.set("config", "otherkey", "x");
-    cfgManager.registerPrimary(srcA);
-    cfgManager.registerPrimary(srcB);
+		srcA.set("config", "otherkey", "x");
+		srcB.set("config", "otherkey", "x");
+		cfgManager.registerPrimary(srcA);
+		cfgManager.registerPrimary(srcB);
 
-    assertKey(cfgManager, "config", "key", "");
+		assertKey(cfgManager, "config", "key", "");
 
-    srcA.set("config", "key", "a");
-    assertKey(cfgManager, "config", "key", "a");
+		srcA.set("config", "key", "a");
+		assertKey(cfgManager, "config", "key", "a");
 
-    srcB.set("config", "key", "b");
-    assertKey(cfgManager, "config", "key", "b");
-  }
+		srcB.set("config", "key", "b");
+		assertKey(cfgManager, "config", "key", "b");
+	}
 
-  @Test
-  void testReloadConfigurationFallback() {
-    MockConfigSource srcA = new MockConfigSource();
-    MockConfigSource srcB = new MockConfigSource();
-    ConfigManager cfgManager = new ConfigManager();
+	@Test
+	void testReloadConfigurationFallback() {
+		MockConfigSource srcA = new MockConfigSource();
+		MockConfigSource srcB = new MockConfigSource();
+		ConfigManager cfgManager = new ConfigManager();
 
-    srcA.set("config", "otherkey", "x");
-    srcB.set("config", "otherkey", "x");
-    cfgManager.registerPrimary(srcA);
-    cfgManager.registerFallback(srcB);
+		srcA.set("config", "otherkey", "x");
+		srcB.set("config", "otherkey", "x");
+		cfgManager.registerPrimary(srcA);
+		cfgManager.registerFallback(srcB);
 
-    assertKey(cfgManager, "config", "key", "");
+		assertKey(cfgManager, "config", "key", "");
 
-    srcA.set("config", "key", "a");
-    assertKey(cfgManager, "config", "key", "a");
+		srcA.set("config", "key", "a");
+		assertKey(cfgManager, "config", "key", "a");
 
-    srcB.set("config", "key", "b");
-    assertKey(cfgManager, "config", "key", "a");
-  }
+		srcB.set("config", "key", "b");
+		assertKey(cfgManager, "config", "key", "a");
+	}
 }
