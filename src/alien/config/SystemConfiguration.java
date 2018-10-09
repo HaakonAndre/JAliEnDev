392a075f00772915e859b255991ea0b69be3739c
Nikola Hardi <nikola.hardi@cern.ch>
2018-10-09 10:57:54 +0200

    Formatting SystemConfiguration

--- src/alien/config/SystemConfiguration.java
+++ src/alien/config/SystemConfiguration.java
@@ -6,9 +6,9 @@ import java.util.Map;
 import lazyj.ExtProperties;
 
 public class SystemConfiguration implements ConfigSource {
-  public Map<String, ExtProperties> getConfiguration() {
-    Map<String, ExtProperties> tmp = new HashMap<String, ExtProperties>();
-    tmp.put("config", new ExtProperties(System.getProperties()));
-    return tmp;
-  }
+	public Map<String, ExtProperties> getConfiguration() {
+		Map<String, ExtProperties> tmp = new HashMap<String, ExtProperties>();
+		tmp.put("config", new ExtProperties(System.getProperties()));
+		return tmp;
+	}
 }
