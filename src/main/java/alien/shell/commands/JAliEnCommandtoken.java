22486b3943a7eb4c117e3a41bcc0fb46fb51d359
Costin Grigoras <costin.grigoras@cern.ch>
2019-07-16 11:14:47 +0200

    Add `host` certificate type and extend parameter checks

--- src/main/java/alien/shell/commands/JAliEnCommandtoken.java
+++ src/main/java/alien/shell/commands/JAliEnCommandtoken.java
@@ -1,6 +1,8 @@
 package alien.shell.commands;
 
+import java.net.InetAddress;
 import java.util.List;
+import java.util.logging.Level;
 
 import alien.api.Dispatcher;
 import alien.api.ServerException;
@@ -31,40 +33,75 @@ public class JAliEnCommandtoken extends JAliEnBaseCommand {
 		super(commander, alArguments);
 
 		try {
-
 			final OptionParser parser = new OptionParser();
 
 			parser.accepts("u").withRequiredArg();
 			parser.accepts("jobid").withRequiredArg();
 			parser.accepts("v").withRequiredArg().ofType(Integer.class);
 			parser.accepts("t").withRequiredArg();
+			parser.accepts("hostname").withRequiredArg();
 
 			final OptionSet options = parser.parse(alArguments.toArray(new String[] {}));
 
-			if (options.has("u")) {
+			if (options.has("u"))
 				requestedUser = (String) options.valueOf("u");
-			}
+
 			if (options.has("t")) {
 				switch ((String) options.valueOf("t")) {
-				case "job":
-					tokentype = TokenCertificateType.JOB_TOKEN;
-					break;
-				case "jobagent":
-					tokentype = TokenCertificateType.JOB_AGENT_TOKEN;
-					break;
-				default:
-					tokentype = TokenCertificateType.USER_CERTIFICATE;
-					break;
+					case "job":
+						tokentype = TokenCertificateType.JOB_TOKEN;
+						break;
+					case "jobagent":
+						tokentype = TokenCertificateType.JOB_AGENT_TOKEN;
+						break;
+					case "host":
+						tokentype = TokenCertificateType.HOST;
+						break;
+					default:
+						tokentype = TokenCertificateType.USER_CERTIFICATE;
+						break;
 				}
 			}
-			if (options.has("v")) {
+
+			validity = tokentype.getMaxValidity();
+
+			if (options.has("v"))
 				validity = ((Integer) options.valueOf("v")).intValue();
+
+			if (validity > tokentype.getMaxValidity()) {
+				commander.printErrln("Reducing the validity from " + validity + " to " + tokentype.getMaxValidity() + " as this is the max allowed value for this type of certificate");
+				validity = tokentype.getMaxValidity();
 			}
 
-			if (tokentype == TokenCertificateType.JOB_TOKEN && options.has("jobid")) {
-				extension = (String) options.valueOf("jobid");
-			}
-		} catch (final OptionException e) {
+			if (tokentype == TokenCertificateType.JOB_TOKEN)
+				if (options.has("jobid"))
+					extension = (String) options.valueOf("jobid");
+				else
+					commander.printErrln("You should pass a job extension for this type of certificate request");
+
+			if (tokentype == TokenCertificateType.HOST)
+				if (options.has("hostname")) {
+					extension = ((String) options.valueOf("hostname")).trim();
+
+					if (extension.length() == 0 || !extension.contains(".")) {
+						commander.printErrln("Please pass a FQDN as hostname instead of `" + extension + "`");
+						setArgumentsOk(false);
+					}
+					else
+						try {
+							InetAddress.getByName(extension);
+						}
+						catch (@SuppressWarnings("unused") Throwable t) {
+							commander.printErrln("hostname `" + extension + "` cannot be resolved");
+							setArgumentsOk(false);
+						}
+				}
+				else {
+					commander.printErrln("You must indicate the hostname for which to issue the certificate!");
+					setArgumentsOk(false);
+				}
+		}
+		catch (final OptionException e) {
 			printHelp();
 			throw e;
 		}
@@ -76,13 +113,16 @@ public class JAliEnCommandtoken extends JAliEnBaseCommand {
 
 		try {
 			tokenreq = Dispatcher.execute(tokenreq);
-		} catch (ServerException e1) {
-			e1.printStackTrace();
+		}
+		catch (final ServerException e1) {
+			logger.log(Level.WARNING, "Cannot get the token you asked for", e1);
+			commander.printErrln("Server didn't execute your request, reason was: " + e1.getMessage());
+			return;
 		}
 
 		// Try to switch user
-		java.security.cert.X509Certificate[] cert = commander.user.getUserCert();
-		String user = commander.user.getDefaultUser();
+		final java.security.cert.X509Certificate[] cert = commander.user.getUserCert();
+		final String user = commander.user.getDefaultUser();
 		AliEnPrincipal switchUser;
 
 		if (requestedUser != null) {
@@ -106,7 +146,11 @@ public class JAliEnCommandtoken extends JAliEnBaseCommand {
 		if (tokenreq != null) {
 			commander.printOut("tokencert", tokenreq.getCertificateAsString());
 			commander.printOut("tokenkey", tokenreq.getPrivateKeyAsString());
+
+			commander.printOutln(UserFactory.transformDN(tokenreq.getCertificate().getSubjectX500Principal().getName()));
+			commander.printOutln();
 			commander.printOut(tokenreq.getCertificateAsString());
+			commander.printOutln();
 			commander.printOut(tokenreq.getPrivateKeyAsString());
 		}
 		else
@@ -118,10 +162,11 @@ public class JAliEnCommandtoken extends JAliEnBaseCommand {
 		commander.printOutln();
 		commander.printOutln(helpUsage("token", "[-options]"));
 		commander.printOutln(helpStartOptions());
-		commander.printOutln(helpOption("-u <user>"));
-		commander.printOutln(helpOption("-v <validity (days)>"));
-		commander.printOutln(helpOption("-t <tokentype>"));
-		commander.printOutln(helpOption("-jobid <jobID>"));
+		commander.printOutln(helpOption("-u <username>", "switch to another role of yours"));
+		commander.printOutln(helpOption("-v <validity (days)>", "default depends on token type"));
+		commander.printOutln(helpOption("-t <tokentype>", "can be one of: job, jobagent, host, user token (default)"));
+		commander.printOutln(helpOption("-jobid <job DN extension>", "expected to be present in a job token"));
+		commander.printOutln(helpOption("-hostname <FQDN>", "required for a host certificate"));
 		commander.printOutln();
 	}
 
