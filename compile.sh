5e6b2fe636bf14cbe244f8ca0d552e8ed6d18617
Costin Grigoras <costin.grigoras@cern.ch>
2019-11-12 11:03:00 +0100

    Fix Tomcat startup from alien-users.jar

--- compile.sh
+++ compile.sh
@@ -46,10 +46,12 @@ generate_users () {
   echo "Preparing alien-users.jar"
   cd "${BUILDDIR}"
   # extract all specified java classes
-  for dependency in ${JALIEN_HOME}/lib/{FarmMonitor.jar,apmon.jar,bcp*.jar,catalina.jar,javax.json-api-*.jar,jline-*.jar,jopt-simple-*.jar,json-simple-*.jar,lazyj.jar,servlet-api.jar,tomcat-*.jar,ca-api*.jar,java-ca-lib*.jar,annotations-api.jar,jaspic-api.jar}; do
+  for dependency in ${JALIEN_HOME}/lib/{FarmMonitor.jar,apmon.jar,bcp*.jar,catalina.jar,javax.json-api-*.jar,jline-*.jar,jopt-simple-*.jar,json-simple-*.jar,lazyj.jar,servlet-api.jar,tomcat-*.jar,ca-api*.jar,java-ca-lib*.jar,annotations-api.jar,jaspic-api.jar,websocket-api.jar}; do
     jar xf ${dependency}
   done
   rm -rf META-INF
+  mkdir -p META-INF/services
+  echo "org.apache.tomcat.websocket.server.WsSci" > META-INF/services/javax.servlet.ServletContainerInitializer
 
   # clean up of previous jar
   rm -f ${JALIEN_HOME}/alien-users.jar
