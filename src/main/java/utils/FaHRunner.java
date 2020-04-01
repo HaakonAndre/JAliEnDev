b17b5ed2d29d3f1df468b164eb9b3193c8ea62e5
Costin Grigoras <costing@gmail.com>
2020-04-01 19:22:19 +0200

    Switch to using the logger facility instead of stderr messages

--- src/main/java/utils/FaHRunner.java
+++ src/main/java/utils/FaHRunner.java
@@ -5,12 +5,16 @@ package utils;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 import alien.api.Dispatcher;
 import alien.api.taskQueue.FaHTask;
 import alien.catalogue.LFN;
 import alien.catalogue.access.AuthorizationFactory;
 import alien.config.ConfigUtils;
+import alien.monitoring.Monitor;
+import alien.monitoring.MonitorFactory;
 import alien.shell.commands.JAliEnCOMMander;
 import alien.user.AliEnPrincipal;
 import alien.user.JAKeyStore;
@@ -23,16 +27,28 @@ import alien.user.UsersHelper;
 public class FaHRunner {
 
 	/**
+	 * logger
+	 */
+	static final Logger logger = ConfigUtils.getLogger(FaHRunner.class.getCanonicalName());
+
+	/**
+	 * ML monitor object
+	 */
+	static final Monitor monitor = MonitorFactory.getMonitor(FaHRunner.class.getCanonicalName());
+
+	/**
 	 * Entry point where the Folding@Home job starts
 	 *
 	 * @param args
 	 * @throws Exception
 	 */
 	public static void main(final String[] args) throws Exception {
+		System.setProperty("jdk.lang.Process.launchMechanism", "FORK");
+
 		ConfigUtils.setApplicationName("FaH");
 
 		if (!JAKeyStore.loadKeyStore()) {
-			System.err.println("No identity found, exiting");
+			logger.log(Level.SEVERE, "No identity found, exiting");
 			return;
 		}
 
@@ -41,20 +57,20 @@ public class FaHRunner {
 		final long currentJobID = ConfigUtils.getConfig().getl("ALIEN_PROC_ID", -1);
 
 		if (currentJobID < 0) {
-			System.err.println("Cannot get the job ID, exiting");
+			logger.log(Level.SEVERE, "Cannot get the job ID, exiting");
 			return;
 		}
 
 		final FaHTask task = Dispatcher.execute(new FaHTask(currentJobID));
 
 		if (task.getSequenceId() < 0) {
-			System.err.println("No task for me, exiting");
+			logger.log(Level.WARNING, "I was not assigned a slot, exiting");
 			return;
 		}
 
 		final String baseFolder = UsersHelper.getDefaultUserDir(account.getDefaultUser() + "/fah/" + task.getSequenceId());
 
-		System.err.println("I was assigned this slot: " + baseFolder);
+		logger.log(Level.INFO, "I was assigned this slot: " + baseFolder);
 
 		final String snapshotArchive = baseFolder + "/snapshot.tar.gz";
 
@@ -63,16 +79,15 @@ public class FaHRunner {
 		final LFN lSnapshot = commander.c_api.getLFN(snapshotArchive);
 
 		if (lSnapshot != null) {
-			System.err.println("A previous snapshot exists: " + lSnapshot.getCanonicalName());
+			logger.log(Level.INFO, "A previous snapshot exists: " + lSnapshot.getCanonicalName());
 
 			try {
 				commander.c_api.downloadFile(snapshotArchive, new File("snapshot.tar.gz"));
 
-				System.err.println("  snapshot downloaded successfully");
+				logger.log(Level.INFO, "Snapshot of " + lSnapshot.getSize() + " bytes was downloaded successfully");
 			}
 			catch (final IOException ioe) {
-				System.err.println("  snapshot cannot be retrieved due to " + ioe.getMessage());
-				System.err.println("  continuing with an empty sandbox");
+				logger.log(Level.WARNING, "Snapshot cannot be retrieved but I will continue with an empty slot anyway", ioe);
 			}
 		}
 
@@ -82,28 +97,36 @@ public class FaHRunner {
 			p.waitFor();
 		}
 		catch (final IOException ioe) {
-			System.err.println("I couldn't run the payload, execution of ./fah.sh failed with:\n" + ioe.getMessage());
+			logger.log(Level.INFO, "I couldn't run the payload, execution of ./fah.sh failed with:\n" + ioe.getMessage());
 			return;
 		}
 
 		// ok, now we have to upload the results, if any
 		final File outputSnapshot = new File("snapshot.tar.gz");
+		final File logFile = new File("log.txt");
+
+		if (logFile.exists())
+			logger.log(Level.INFO, "Job produced a log file, it was probably ok");
+		else {
+			logger.log(Level.SEVERE, "Job didn't run correctly, no log.txt file found");
+			return;
+		}
 
 		if (outputSnapshot.exists() && outputSnapshot.length() > 100000) {
-			System.err.println("Uploading intermediate work to " + outputSnapshot);
+			logger.log(Level.INFO, "Uploading intermediate work to " + outputSnapshot);
 
 			if (lSnapshot != null) {
 				if (commander.c_api.removeLFN(snapshotArchive))
-					System.err.println("  removal of previous archive was successful");
+					logger.log(Level.INFO, "Removal of previous archive was successful");
 				else
-					System.err.println("  could not remove the previous archive");
+					logger.log(Level.WARNING, "Could not remove the previous archive, has somebody removed it while we were running?");
 			}
 
-			System.err.println("  uploading " + outputSnapshot.length() + " bytes");
+			logger.log(Level.INFO, "Uploading " + outputSnapshot.length() + " bytes");
 			commander.c_api.uploadFile(outputSnapshot, snapshotArchive, "-w", "-T", "2", "-d");
-			System.err.println("  upload complete");
+			logger.log(Level.INFO, "Upload complete");
 		}
 		else
-			System.err.println("No output to upload");
+			logger.log(Level.INFO, "No output to upload");
 	}
 }
