46388af7bb27dd35612a49d68ab71c5db23a1bf1
Costin Grigoras <costing@gmail.com>
2020-04-25 13:22:42 +0200

    Performance of CS is affected when this flag is used

--- src/main/java/alien/config/ConfigUtils.java
+++ src/main/java/alien/config/ConfigUtils.java
@@ -159,7 +159,8 @@ public class ConfigUtils {
 	}
 
 	private static void setSystemDefaults() {
-		System.setProperty("jdk.lang.Process.launchMechanism", "FORK");
+		if (!isCentralService())
+			System.setProperty("jdk.lang.Process.launchMechanism", "FORK");
 	}
 
 	static {
