5e1b641a933bea9e312697c94a1e666265a7c7fb
Costin Grigoras <costing@gmail.com>
2020-06-08 17:24:39 +0200

    Add the `changeDiff` command to quickly see what changed with an `edit`

--- src/main/java/alien/shell/commands/JAliEnCOMMander.java
+++ src/main/java/alien/shell/commands/JAliEnCOMMander.java
@@ -93,7 +93,7 @@ public class JAliEnCOMMander extends Thread {
 			"find_csd", "listFilesFromCollection", "submit", "motd", "access", "commit", "packages", "pwd", "ps", "rmdir", "rm", "rm_csd", "mv", "mv_csd", "masterjob", "user", "touch", "touch_csd",
 			"type", "kill", "lfn2guid", "guid2lfn", "guid2lfn_csd", "w", "uptime", "addFileToCollection", "chgroup", "chown", "chown_csd", "deleteMirror", "df", "du", "fquota", "jquota",
 			"listSEDistance", "listTransfer", "md5sum", "mirror", "resubmit", "top", "groups", "token", "uuid", "stat", "listSEs", "xrdstat", "whois", "ping", "setSite", "grep", "showTagValue",
-			"randomPFNs", "toXml" };
+			"randomPFNs", "toXml", "changeDiff" };
 
 	private static final String[] jAliEnAdminCommandList = new String[] { "queue", "register", "groupmembers" };
 
