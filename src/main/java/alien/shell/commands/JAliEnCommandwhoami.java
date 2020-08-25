47de38caeee263cde7668bd7f97de7f68e389a5c
Costin Grigoras <costing@gmail.com>
2020-08-25 21:43:18 +0200

    Print more information with the `-v` switch

--- src/main/java/alien/shell/commands/JAliEnCommandwhoami.java
+++ src/main/java/alien/shell/commands/JAliEnCommandwhoami.java
@@ -2,39 +2,69 @@ package alien.shell.commands;
 
 import java.time.ZoneId;
 import java.time.ZonedDateTime;
+import java.util.ArrayList;
 import java.util.List;
 
 import alien.user.AliEnPrincipal;
+import joptsimple.OptionException;
+import joptsimple.OptionParser;
+import joptsimple.OptionSet;
+import lazyj.Format;
 
 /**
  * Some info about the current identity
- * 
+ *
  * @author costing
  * @since Aug 17, 2020
  */
 public class JAliEnCommandwhoami extends JAliEnBaseCommand {
 
+	private boolean verbose = false;
+
 	@Override
 	public void run() {
 		final AliEnPrincipal user = commander.getUser();
-
-		// simple shell printout
-		commander.printOutln(user.getName());
 
 		// all other current user info is available to the API only
 		commander.printOut("username", user.getName());
 		commander.printOut("role", user.getDefaultRole());
 		commander.printOut("roles", String.join(",", user.getRoles()));
 
+		if (verbose) {
+			commander.printOutln("Username: " + user.getName());
+
+			final ArrayList<String> otherRoles = new ArrayList<>(user.getRoles());
+
+			otherRoles.remove(user.getDefaultRole());
+
+			commander.printOut("Role: " + user.getDefaultRole());
+
+			if (otherRoles.size() > 0)
+				commander.printOut(" (other roles: " + String.join(",", otherRoles) + ")");
+
+			commander.printOutln();
+		}
+		else
+			// simple shell printout
+			commander.printOutln(user.getName());
+
 		if (user.getUserCert() != null) {
 			final ZonedDateTime userNotAfter = user.getUserCert()[0].getNotAfter().toInstant().atZone(ZoneId.systemDefault());
 
 			commander.printOut("certificate_expires", String.valueOf(userNotAfter.toEpochSecond()));
 			commander.printOut("certificate_dn", user.getUserCert()[0].getSubjectDN().toString());
+
+			if (verbose)
+				commander.printOutln("Certificate DN: " + user.getUserCert()[0].getSubjectDN().toString() + " (expiring on " + userNotAfter + ", which is in "
+						+ Format.toInterval(userNotAfter.toEpochSecond() * 1000L - System.currentTimeMillis()) + ")");
 		}
 
-		if (user.getRemoteEndpoint() != null)
+		if (user.getRemoteEndpoint() != null) {
 			commander.printOut("connected_from", user.getRemoteEndpoint().toString());
+
+			if (verbose)
+				commander.printOutln("Connected from: " + user.getRemoteEndpoint());
+		}
 	}
 
 	/**
@@ -44,7 +74,8 @@ public class JAliEnCommandwhoami extends JAliEnBaseCommand {
 	public void printHelp() {
 		commander.printOutln();
 		commander.printOutln(helpUsage("whoami", ""));
-		commander.printOutln();
+		commander.printOutln(helpStartOptions());
+		commander.printOutln(helpOption("-v", "verbose details of the current identity"));
 	}
 
 	/**
@@ -67,5 +98,18 @@ public class JAliEnCommandwhoami extends JAliEnBaseCommand {
 	 */
 	public JAliEnCommandwhoami(final JAliEnCOMMander commander, final List<String> alArguments) {
 		super(commander, alArguments);
+
+		try {
+			final OptionParser parser = new OptionParser();
+
+			parser.accepts("v");
+
+			final OptionSet options = parser.parse(alArguments.toArray(new String[] {}));
+
+			verbose = options.has("v");
+		}
+		catch (@SuppressWarnings("unused") final OptionException | IllegalArgumentException e) {
+			setArgumentsOk(false);
+		}
 	}
 }
