00e24ed7f039ad724c3d086e42169ada355a5424
Max <mstoretv@cern.ch>
2020-08-26 10:38:39 +0200

    Restore generic executeCommand

--- src/main/java/alien/site/batchqueue/BatchQueue.java
+++ src/main/java/alien/site/batchqueue/BatchQueue.java
@@ -4,6 +4,8 @@ import java.io.BufferedReader;
 import java.io.StringReader;
 import java.util.HashMap;
 import java.util.logging.Logger;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 import java.util.ArrayList;
 import java.util.Map;
 import java.util.concurrent.TimeUnit;
@@ -60,6 +62,9 @@ public abstract class BatchQueue {
 
 	public ArrayList<String> executeCommand(String cmd) {
 		ArrayList<String> proc_output = new ArrayList<>();
+
+		logger.info("Executing: " + cmd);
+
 		try {
 			ArrayList<String> cmd_full = new ArrayList<>();
 			cmd_full.add("/bin/bash");
@@ -68,14 +73,47 @@ public abstract class BatchQueue {
 			final ProcessBuilder proc_builder = new ProcessBuilder(cmd_full);
 
 			Map<String, String> env = proc_builder.environment();
-			env.clear();
+			String[] dirs = {
+				"/cvmfs/alice.cern.ch/",
+				env.get("JALIEN_ROOT"),
+				env.get("JALIEN_HOME"),
+				env.get("ALIEN_ROOT"),
+			};
 
-			env.putAll(additional_env_vars);
+			HashMap<String, String> cleaned_env_vars = new HashMap<>();
+			Pattern p = Pattern.compile(".*PATH$");
 
-			proc_builder.redirectErrorStream(false);
+			for (String var : env.keySet()) {
+				Matcher m = p.matcher(var);
+
+				if (!m.matches()) {
+					continue;
+				}
+
+				String val = env.get(var);
+
+				//
+				// remove any traces of (J)AliEn...
+				//
+
+				for (String d : dirs) {
+					if (d == null) {
+						continue;
+					}
+
+					String dir = d.replaceAll("/+$", "");
+					String pat = "\\Q" + dir + "\\E/[^:]*:?";
+					val = val.replaceAll(pat, "");
+				}
+
+				cleaned_env_vars.put(var, val);
+			}
+
+			env.putAll(cleaned_env_vars);
+
+			proc_builder.redirectErrorStream(true);
 
 			final Process proc = proc_builder.start();
-
 			final ProcessWithTimeout pTimeout = new ProcessWithTimeout(proc, proc_builder);
 
 			pTimeout.waitFor(60, TimeUnit.SECONDS);
@@ -83,20 +121,18 @@ public abstract class BatchQueue {
 			final ExitStatus exitStatus = pTimeout.getExitStatus();
 			logger.info("Process exit status: " + exitStatus.getExecutorFinishStatus());
 
-			if (exitStatus.getExtProcExitStatus() == 0) {
-				final BufferedReader reader = new BufferedReader(new StringReader(exitStatus.getStdOut()));
+			final BufferedReader reader = new BufferedReader(new StringReader(exitStatus.getStdOut()));
+			String output_str;
 
-				String output_str;
-
-				while ((output_str = reader.readLine()) != null)
-					proc_output.add(output_str.trim());
+			while ((output_str = reader.readLine()) != null) {
+				proc_output.add(output_str.trim());
 			}
-		}
-		catch (final Throwable t) {
+		} catch (final Throwable t) {
 			logger.log(Level.WARNING, "Exception executing command: " + cmd, t);
 		}
-		this.logger.info("[BatchQueue] Command output: " + proc_output);
+
 		return proc_output;
+
 	}
 
 	public final String getValue(final String keyValue, final String key, final String defaultValue){
