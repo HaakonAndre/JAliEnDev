83491d1e7ef0785e8566502bc1e2cc32aac3c1b5
Maarten Litmaath <Maarten.Litmaath@cern.ch>
2020-09-19 17:54:24 +0200

    Clean up remnants from earlier testing.

--- src/main/java/alien/site/batchqueue/ARC.java
+++ src/main/java/alien/site/batchqueue/ARC.java
@@ -27,9 +27,6 @@ import javax.naming.directory.InitialDirContext;
 import javax.naming.directory.SearchControls;
 import javax.naming.directory.SearchResult;
 
-/*
- * import alien.test.utils.Functions;
- */
 import alien.site.Functions;
 
 /**
@@ -730,82 +727,4 @@ public class ARC extends BatchQueue {
 		logger.info("Kill command not implemented");
 		return 0;
 	}
-
-	/*
-	 * START - temporarily included here: * /
-	 * private ArrayList<String> executeCommand(String cmd) {
-	 * ArrayList<String> proc_output = new ArrayList<>();
-	 * 
-	 * logger.info("Executing: " + cmd);
-	 * 
-	 * try {
-	 * ArrayList<String> cmd_full = new ArrayList<>();
-	 * cmd_full.add("/bin/bash");
-	 * cmd_full.add("-c");
-	 * cmd_full.add(cmd);
-	 * final ProcessBuilder proc_builder = new ProcessBuilder(cmd_full);
-	 * 
-	 * Map<String, String> env = proc_builder.environment();
-	 * String[] dirs = {
-	 * "/cvmfs/alice.cern.ch/",
-	 * env.get("JALIEN_ROOT"),
-	 * env.get("JALIEN_HOME"),
-	 * env.get("ALIEN_ROOT"),
-	 * };
-	 * 
-	 * HashMap<String, String> cleaned_env_vars = new HashMap<>();
-	 * Pattern p = Pattern.compile(".*PATH$");
-	 * 
-	 * for (String var : env.keySet()) {
-	 * Matcher m = p.matcher(var);
-	 * 
-	 * if (!m.matches()) {
-	 * continue;
-	 * }
-	 * 
-	 * String val = env.get(var);
-	 * 
-	 * //
-	 * // remove any traces of (J)AliEn...
-	 * //
-	 * 
-	 * for (String d : dirs) {
-	 * if (d == null) {
-	 * continue;
-	 * }
-	 * 
-	 * String dir = d.replaceAll("/+$", "");
-	 * String pat = "\\Q" + dir + "\\E/[^:]*:?";
-	 * val = val.replaceAll(pat, "");
-	 * }
-	 * 
-	 * cleaned_env_vars.put(var, val);
-	 * }
-	 * 
-	 * env.putAll(cleaned_env_vars);
-	 * 
-	 * proc_builder.redirectErrorStream(true);
-	 * 
-	 * final Process proc = proc_builder.start();
-	 * final ProcessWithTimeout pTimeout = new ProcessWithTimeout(proc, proc_builder);
-	 * 
-	 * pTimeout.waitFor(60, TimeUnit.SECONDS);
-	 * 
-	 * final ExitStatus exitStatus = pTimeout.getExitStatus();
-	 * logger.info("Process exit status: " + exitStatus.getExecutorFinishStatus());
-	 * 
-	 * final BufferedReader reader = new BufferedReader(new StringReader(exitStatus.getStdOut()));
-	 * String output_str;
-	 * 
-	 * while ((output_str = reader.readLine()) != null) {
-	 * proc_output.add(output_str.trim());
-	 * }
-	 * } catch (final Throwable t) {
-	 * logger.warning("Exception executing command: " + cmd);
-	 * }
-	 * 
-	 * return proc_output;
-	 * }
-	 * /* STOP - temporarily included here
-	 */
 }
